2eada78c4ea087e1f13f1bfff13d331b87981733
Zul <22528471@stu.mmu.ac.uk>
2025-10-01 17:09:53 +0100

    Created LayerUI and UI class

---
+++ Engine/src/LayerSystem/Layers/LayerUI.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "LayerSystem/Layer.h"
+
+namespace IonixEngine
+{
+    class LayerEditor : public Layer
+    {
+    public:
+        LayerEditor() {}
+
+        virtual void OnAttach() override;
+        virtual void OnDetach() override;
+        virtual void OnUpdate() override;
+        virtual void OnEvent(IonixEvent& e) override;
+
+    private:
+        void OnWindowClosedEvent(WindowClosedEvent& e);
+    };
+}
