7a9c3f2ec1a91cd98870c8b9a967d66bfbaeb185
Daniel Cleaver <23665045@stu.mmu.ac.uk>
2025-10-07 10:11:17 +0100

    Created instance of scripting in Application.cpp

---
+++ Engine/src/Scripting/Scripting.cpp
@@ -0,0 +1,16 @@
+#include "Scripting/Scripting.h"
+
+namespace IonixEngine 
+{
+	Scripting* Scripting::s_Instance = nullptr;
+
+	Scripting& Scripting::Get() 
+	{
+		if (!s_Instance) 
+		{
+			s_Instance = new Scripting();
+		}
+
+		return *s_Instance;
+	}
+}
