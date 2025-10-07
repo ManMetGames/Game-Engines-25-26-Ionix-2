997f02930cbe73230b98409c4995d7a34dae326e
Clay Warden <claysonwarden@gmail.com>
2025-10-07 10:46:44 +0100

    keyboard: added keycodes
    keycodes have been added

--- Engine/src/EventSystem/Event.h
+++ Engine/src/EventSystem/Event.h
@@ -11,7 +11,7 @@ namespace IonixEngine
         // ...
     };
 
-    // keycodes. yay
+    // scancodes
     typedef enum SDL_Scancode
     {
         SDL_SCANCODE_UNKNOWN = 0,
@@ -63,6 +63,64 @@ namespace IonixEngine
         SDL_SCANCODE_LEFT = 80,
         SDL_SCANCODE_DOWN = 81,
         SDL_SCANCODE_UP = 82
+    };
+
+    // keycodes
+    typedef enum SDL_Keycode
+    {
+        SDLK_UNKNOWN = 0,
+
+        // alphabet
+        SDLK_a = 'a',
+        SDLK_b = 'b',
+        SDLK_c = 'c',
+        SDLK_d = 'd',
+        SDLK_e = 'e',
+        SDLK_f = 'f',
+        SDLK_g = 'g',
+        SDLK_h = 'h',
+        SDLK_i = 'i',
+        SDLK_j = 'j',
+        SDLK_k = 'k',
+        SDLK_l = 'l',
+        SDLK_m = 'm',
+        SDLK_n = 'n',
+        SDLK_o = 'o',
+        SDLK_p = 'p',
+        SDLK_q = 'q',
+        SDLK_r = 'r',
+        SDLK_s = 's',
+        SDLK_t = 't',
+        SDLK_u = 'u',
+        SDLK_v = 'v',
+        SDLK_w = 'w',
+        SDLK_x = 'x',
+        SDLK_y = 'y',
+        SDLK_z = 'z',
+
+        //numbers
+        SDLK_1 = '1',
+        SDLK_2 = '2',
+        SDLK_3 = '3',
+        SDLK_4 = '4',
+        SDLK_5 = '5',
+        SDLK_6 = '6',
+        SDLK_7 = '7',
+        SDLK_8 = '8',
+        SDLK_9 = '9',
+        SDLK_0 = '0',
+
+
+        //other keys
+        //SDLK_TAB = '\t',
+        //SDLK_SPACE = ' ',
+        //SDLK_RIGHT = "",
+        //SDLK_LEFT = 80,
+        //SDLK_DOWN = 81,
+        //SDLK_UP = 82
+        //this will be done at a later time
+
+
     };
 
     // Event class
