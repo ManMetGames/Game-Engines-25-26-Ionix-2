4421afdca256a430493a5b290fe8bedf6042dfde
whirlstrike <c.c.j.w.cw@gmail.com>
2025-10-07 15:46:22 +0100

    audio format check
    a way to check if the format is the correct one, untested

--- Engine/src/LayerSystem/Layers/Audio.h
+++ Engine/src/LayerSystem/Layers/Audio.h
@@ -12,7 +12,14 @@ namespace IonixEngine
         void Audio::PlayAudio(const char* file, int loops)
         {
             Mix_Chunk* chunk = Mix_LoadWAV(file);
+            //check if audio format is an MP3 or WAV format
                 if (!chunk) return;
+                //needs testing on pc with audio output /Joshua walker
+                //if (chunk == NULL)
+                //{
+                //    SDL_Log("error incorect audio format, use MP3 or Wav", Mix_GetError());
+                //    return;
+                //}
 
             Mix_PlayChannel(-1, chunk, loops);
         }
