b28946b00a309f1a35c761b382384f41ff1de6a1
JakeHenderson79 <23626171@stu.mmu.ac.uk>
2025-10-14 16:49:50 +0100

    Started Implementation
    Started work on adding the Prismatic, Weld and Pulley Joint classes

--- Engine/src/Fysics/Joints.h
+++ Engine/src/Fysics/Joints.h
@@ -11,7 +11,47 @@ namespace IonixEngine
         {
             world = LayerFysics::GetInstance()->GetWorld();
         }
+
+        /*void SetPrismaticJoint(b2Body *bodyA, b2Body *bodyB) {
+            b2PrismaticJointDef jointDef;
+            b2Vec2 worldAxis(1.0f, 0.0f);
+            jointDef.Initialize(bodyA, bodyB, bodyA->GetWorldCenter(), worldAxis);
+            jointDef.lowerTranslation = -5.0f;
+            jointDef.upperTranslation = 2.5f;
+            jointDef.enableLimit = true;
+            jointDef.maxMotorForce = 1.0f;
+            jointDef.motorSpeed = 0.0f;
+            jointDef.enableMotor = true;
+
+            
+        }
+
+        void SetWeldJoint() {
+
+        }
+
+        void SetPulleyJoint() {
+
+        }
+
+        void DestroyJoint() {
+
+        }*/
     };
+
+    class PrismaticJoints {
+
+    };
+
+    class WeldJoints {
+
+    };
+
+    class PulleyJoints {
+
+    };
+
+   
     
 }
 
