81957f3a13a069adc0275eb7a1dc8edfcb3e9593
Joseph Hill <91340791+Subluxx@users.noreply.github.com>
2025-10-14 10:04:38 +0100

    completed World Setup
    done yippie xoxoxo

--- Engine/src/LayerSystem/Layers/LayerFysics.cpp
+++ Engine/src/LayerSystem/Layers/LayerFysics.cpp
@@ -14,8 +14,15 @@ namespace IonixEngine
         //Create the world
         b2World world(gravity);
     }
-    void LayerFysics::OnDetach() {}
-    void LayerFysics::OnUpdate(){} 
+    void LayerFysics::OnDetach() 
+    {
+        delete world; world = nullptr;
+    }
+    void LayerFysics::OnUpdate()
+    {
+        if (!world) return; //cheeky early return to see if world is attatched
+        world->Step(timeStep, velocityIterations, positionIterations); // world step on update
+    } 
     void LayerFysics::OnEvent(IonixEvent& e)
     {
         // Switch statement routes the event and invokes the relevant event handler
@@ -25,5 +32,17 @@ namespace IonixEngine
             // Add more cases as needed.... (Note: Most engine features don't require events, they
             //                              can just be callable functions.
         }
+    }
+
+    void LayerFysics::SetGravity(float x, float y, bool wake)
+    {
+        if (!world) return;
+        world->SetGravity(b2Vec2(x, y));
+        if (wake) for (b2Body* b = world->GetBodyList();b;b = b->GetNext()) b->SetAwake(true);
+    }
+
+    b2Vec2 LayerFysics::GetGravity() const
+    {
+        return world ? world->GetGravity() : b2Vec2(0.f, 0.f);
     }
 }
--- Engine/src/LayerSystem/Layers/LayerFysics.h
+++ Engine/src/LayerSystem/Layers/LayerFysics.h
@@ -13,5 +13,13 @@ namespace IonixEngine
         virtual void OnDetach() override;
         virtual void OnUpdate() override;
         virtual void OnEvent(IonixEvent& e) override;
+        virtual void SetGravity(float x, float y, bool wake = true);
+        virtual b2Vec2 GetGravity() const;
+    private:
+        b2World* world{ nullptr };
+        float timeStep{ 1.f / 60.f };
+        int32 velocityIterations{ 6 };
+        int32 positionIterations{ 2 };
+        float ppm{ 100.f };
     };
 }
