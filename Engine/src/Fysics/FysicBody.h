6dbb9a24af7e98181a9bc9d08af1e8d218fd7f9b
bentatam <146729919+bentatam@users.noreply.github.com>
2025-10-14 13:32:54 +0100

    FysicBodyCreation
    Started work on creating fysicBodies

--- Engine/src/Fysics/FysicBody.h
+++ Engine/src/Fysics/FysicBody.h
@@ -1,15 +1,31 @@
 #pragma once
+#include <sol/sol.hpp>
+
 #include "LayerSystem/Layers/LayerFysics.h"
 
 namespace IonixEngine
 {
+    enum class fysicBodyType {staticBody, dynamicBody, kinematicBody  };
+    
     class FysicBody
     {
         b2World* world;
 
-        FysicBody()
+        fysicBodyType type;
+        switch (type)
+        {
+        case fysicBodyType::staticBody:
+            
+        }
+        
+        FysicBody(float xPos, float yPos, enum fysicBodyType)
         {
             world = LayerFysics::GetInstance()->GetWorld();
+
+            b2BodyDef bodyDef;
+            bodyDef.type = b2;
+            bodyDef.position.Set(xPos, yPos);
+            b2Body* body = world.CreateBody(&bodyDef);
         }
     };
     
