7fef788b95ea292a5876c843c0b0e1a5f3e473f7
JakeHenderson79 <23626171@stu.mmu.ac.uk>
2025-10-18 14:34:47 +0100

    Added Prismatic Joint + base Joint functions

--- Engine/src/Fysics/Joints.h
+++ Engine/src/Fysics/Joints.h
@@ -3,44 +3,93 @@
 
 namespace IonixEngine
 {
-    class Joints
+     class Joints    
     {
-        b2World* world;
+    public:
+        b2World* world;    
 
+     
+
+        b2JointUserData userData;
         Joints()
         {
-            world = LayerFysics::GetInstance()->GetWorld();
+            world = LayerFysics::GetInstance()->GetWorld();          
         }
 
-        /*void SetPrismaticJoint(b2Body *bodyA, b2Body *bodyB) {
-            b2PrismaticJointDef jointDef;
-            b2Vec2 worldAxis(1.0f, 0.0f);
-            jointDef.Initialize(bodyA, bodyB, bodyA->GetWorldCenter(), worldAxis);
-            jointDef.lowerTranslation = -5.0f;
-            jointDef.upperTranslation = 2.5f;
-            jointDef.enableLimit = true;
-            jointDef.maxMotorForce = 1.0f;
-            jointDef.motorSpeed = 0.0f;
-            jointDef.enableMotor = true;
+   
+        virtual void setJoint(b2Body* bodyA, b2Body* bodyB);       
 
-            
+        virtual void deleteJoint();
+    
+        b2Body* getBodyA(b2Joint* joint) {
+            return joint->GetBodyA();
+        }
+     
+        b2Body* getBodyB(b2Joint* joint) {
+            return joint->GetBodyB();
         }
 
-        void SetWeldJoint() {
-
+        b2Vec2 getAnchorA(b2Joint* joint) {
+            return joint->GetAnchorA();
         }
 
-        void SetPulleyJoint() {
-
+        b2Vec2 getAnchorB(b2Joint* joint) {
+            return joint->GetAnchorB();
         }
 
-        void DestroyJoint() {
+        void getUserData(b2Joint* joint) {
+           userData = joint->GetUserData();
+        }
 
-        }*/
+       
     };
 
-    class PrismaticJoints {
+    class PrismaticJoints : public Joints{
+    public:
 
+        PrismaticJoints() : Joints() {}
+        b2PrismaticJoint* joint;
+
+      void setJoint(b2Body* bodyA, b2Body* bodyB) override {
+         b2PrismaticJointDef jointDef;
+         b2Vec2 worldAxis(1.0f, 0.0f);
+         jointDef.Initialize(bodyA, bodyB, bodyA->GetWorldCenter(), worldAxis);
+         jointDef.lowerTranslation = -5.0f;
+         jointDef.upperTranslation = 2.5f;
+         jointDef.enableLimit = true;
+         jointDef.maxMotorForce = 1.0f;
+         jointDef.motorSpeed = 0.0f;
+         jointDef.enableMotor = true;
+
+         joint = (b2PrismaticJoint*)world->CreateJoint(&jointDef);
+
+      }
+      b2Joint* getJoint() {
+          return joint;
+      }
+      void deleteJoint() override {
+          world->DestroyJoint(joint);
+      }
+
+      float getJointTranslation() {
+          return joint->GetJointTranslation();
+      }
+    
+      float getJointSpeed() {
+          return joint->GetJointSpeed();
+      }
+
+      float getMotorForce() {
+          return joint->GetMotorForce(60.0f); //requires inverse delta time to be made
+      }
+
+      void SetMotorSpeed(float speed) {
+          joint->SetMotorSpeed(speed);
+      }
+
+      void SetMotorForce(float force) {
+          joint->SetMaxMotorForce(force);
+      }
     };
 
     class WeldJoints {
@@ -50,8 +99,6 @@ namespace IonixEngine
     class PulleyJoints {
 
     };
-
-   
     
 }
 
