5a3514174af55400906b2cf0149c85446bb7b61b
bentatam <22511467@stu.mmu.ac.uk>
2025-10-19 19:37:00 +0100

    Polished constructors Slightly

--- Engine/src/Fysics/FysicBody.h
+++ Engine/src/Fysics/FysicBody.h
@@ -7,11 +7,13 @@ namespace IonixEngine
 {
     enum class fysicBodyType {staticBody, dynamicBody, kinematicBody};
     
-    class CreateFysicBody
+    class FysicBody
     {
+    private:
         b2World* world;
-
-        CreateFysicBody()
+        b2Body* body;
+    public:
+        FysicBody()
         {
             world = LayerFysics::GetInstance()->GetWorld();
             b2BodyDef bodyDef;
@@ -19,10 +21,10 @@ namespace IonixEngine
             bodyDef.position.Set(0, 0);
             bodyDef.awake = true;
             bodyDef.fixedRotation = false;
-            b2Body* body = world->CreateBody(&bodyDef);
+            body = world->CreateBody(&bodyDef);
         }
         
-        CreateFysicBody(float xPos, float yPos, fysicBodyType b_type, bool rotationLocked)
+        FysicBody(float xPos, float yPos, fysicBodyType b_type, bool rotationLocked)
         {
             world = LayerFysics::GetInstance()->GetWorld();
             b2BodyDef bodyDef;
@@ -41,7 +43,30 @@ namespace IonixEngine
             bodyDef.position.Set(xPos, yPos);
             bodyDef.awake = true;
             bodyDef.fixedRotation = rotationLocked;
-            b2Body* body = world->CreateBody(&bodyDef);
+            body = world->CreateBody(&bodyDef);
+        }
+
+        FysicBody(float xPos, float yPos, fysicBodyType b_type, bool rotationLocked, float gravityScale)
+        {
+            world = LayerFysics::GetInstance()->GetWorld();
+            b2BodyDef bodyDef;
+            switch (b_type)
+            {
+            case fysicBodyType::staticBody:
+                bodyDef.type = b2_staticBody;
+                break;
+            case fysicBodyType::dynamicBody:
+                bodyDef.type = b2_dynamicBody;
+                break;
+            case fysicBodyType::kinematicBody:
+                bodyDef.type = b2_kinematicBody;
+                break;
+            }
+            bodyDef.position.Set(xPos, yPos);
+            bodyDef.awake = true;
+            bodyDef.fixedRotation = rotationLocked;
+            bodyDef.gravityScale = gravityScale;
+            body = world->CreateBody(&bodyDef);
         }
     };
 }
