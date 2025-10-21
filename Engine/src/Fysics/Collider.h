27f65a2b959b30d1ba5e6ec26cd445acdd1793cf
JoshuaJ123 <22512619@stu.mmu.ac.uk>
2025-10-21 16:37:24 +0100

    updated collider

--- Engine/src/Fysics/Collider.h
+++ Engine/src/Fysics/Collider.h
@@ -35,7 +35,7 @@ namespace IonixEngine
         Contact getContact(const Rect& a, const Rect& b)
         {
             //Creates an empty contact and gives variables values
-            Contact c{};
+            Contact contact{};
             float aCentreX = a.x + a.width / 2;
             float aCentreY = a.y + a.height / 2;
             float bCentreX = b.x + b.width / 2;
@@ -44,35 +44,35 @@ namespace IonixEngine
             //Checks for no overlap case
             float dx = bCentreX - aCentreX;
             float overlapX = (a.width / 2 + b.width / 2) - Maf::mafAbs(dx);
-            if (overlapX <= 0) return c;
+            if (overlapX <= 0) return contact;
 
 
             float dy = bCentreY - aCentreY;
             float overlapY = (a.height / 2 + b.height / 2) - Maf::mafAbs(dy);
-            if (overlapY <= 0) return c;
+            if (overlapY <= 0) return contact;
 
             //An overlap is detected.
-            c.isTouching = true;
+            contact.isTouching = true;
 
             //Picking the smallest overlap
             if (overlapX < overlapY)
             {
                 //Determins the direction of the normal to apply force in the appropriate direction
-                c.overlapX = (dx < 0 ? -overlapX : overlapX);
-                c.overlapY = 0;
-                c.normalX = (dx < 0 ? -1 : 1);
-                c.normalY = 0;
+                contact.overlapX = (dx < 0 ? -overlapX : overlapX);
+                contact.overlapY = 0;
+                contact.normalX = (dx < 0 ? -1 : 1);
+                contact.normalY = 0;
             }
             else
             {
                 //Determins the direction of the normal to apply force in the appropriate direction
-                c.overlapX = 0;
-                c.overlapY = (dy < 0 ? -overlapY : overlapY);
-                c.normalX = 0;
-                c.normalY = (dy < 0 ? -1 : 1);
+                contact.overlapX = 0;
+                contact.overlapY = (dy < 0 ? -overlapY : overlapY);
+                contact.normalX = 0;
+                contact.normalY = (dy < 0 ? -1 : 1);
             }
             //Returns the Contact point and the direction of the normal
-            return c;
+            return contact;
         }
 
         struct Circle {           //Circle struct
@@ -92,31 +92,31 @@ namespace IonixEngine
         Contact getContact(const Circle& a, const Circle& b)
         {
             //Creates an empty contact and gives variables values
-            Contact c{};
+            Contact contact{};
             float dx = a.x - b.x;
             float dy = a.y - b.y;
             float distanceSqr = dx * dx + dy * dy;
             float radiusSum = a.radius + b.radius;
 
             //Checks for no overlap case
-            if (distanceSqr >= radiusSum * radiusSum) return c;
+            if (distanceSqr >= radiusSum * radiusSum) return contact;
 
             //An overlap is detected.
-            c.isTouching = true;
+            contact.isTouching = true;
 
             //Determins the true distance between the two circles
             float dist = Maf::mafSqrt(distanceSqr);
             //Computes the depth
             float overlap = radiusSum - dist;
             //Creates a unit vector for the collision normal
-            c.normalX = dx / dist;
-            c.normalY = dy / dist;
+            contact.normalX = dx / dist;
+            contact.normalY = dy / dist;
             //Works out the required distance to move a circle to seperate them
-            c.overlapX = c.normalX * overlap;
-            c.overlapY = c.normalY * overlap;
+            contact.overlapX = contact.normalX * overlap;
+            contact.overlapY = contact.normalY * overlap;
 
             //Returns the Contact point and the direction of the normal
-            return c;
+            return contact;
         }
     };
 }
