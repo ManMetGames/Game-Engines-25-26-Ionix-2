8306b52eeae3687027d47a5773878a2bdc7c9ae9
Ollie Pan <olypann@gmail.com>
2025-10-21 01:31:07 +0100

    Implemented AddCircle method

--- Engine/src/Fysics/Shapes.h
+++ Engine/src/Fysics/Shapes.h
@@ -1,17 +1,54 @@
 #pragma once
 #include "LayerSystem/Layers/LayerFysics.h"
 
+#include <sol/sol.hpp>
+
+
+
 namespace IonixEngine
 {
-    class Shapes
-    {
-        b2World* world;
+    enum class fysicShapeType{circle, box, polygon};
 
-        Shapes()
-        {
-            world = LayerFysics::GetInstance()->GetWorld();
-        }
+    class FysicsShapes
+    {
+        private:
+            b2World* world;
+            b2Fixture* fixture;
+            b2Body* body;
+
+        public:
+            //world = LayerFysics::GetInstance()->GetWorld();
+            FysicsShapes(b2Body* attachedBody)
+            {
+                body = attachedBody;
+                fixture = nullptr;
+            }
+
+            ~FysicsShapes()
+            {
+                if (body && fixture)
+                {
+                    body->DestroyFixture(fixture);
+                    fixture = nullptr;
+                }
+            }
+
+            void AddCircle(float radius, b2Vec2 offset = { 0.0f, 0.0f }, bool isTrigger = false)
+            {
+                b2CircleShape shape;
+
+                shape.m_radius = radius;
+                shape.m_p = offset;
+
+                b2FixtureDef fixtureDef;
+
+                fixtureDef.shape = &shape;
+                fixtureDef.isSensor = isTrigger;
+
+                fixture = body->CreateFixture(&fixtureDef);
+            }
     };
+
     
 }
 
