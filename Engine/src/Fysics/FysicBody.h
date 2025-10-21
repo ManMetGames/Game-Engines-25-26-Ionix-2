7785ec5621f5de6a3445a8f91576108b9123e6d0
bentatam <146729919+bentatam@users.noreply.github.com>
2025-10-21 14:50:11 +0100

    Moved Force to correct header File

--- Engine/src/Fysics/FysicBody.h
+++ Engine/src/Fysics/FysicBody.h
@@ -162,52 +162,7 @@ namespace IonixEngine
         
         //Add Force
         
-        void AddForce(b2Vec2 force, b2Vec2 point)
-        {
-            if (body)
-            {
-                body->ApplyForce(force, point, true);
-            }
-        }
-
-        void AddForceToCenter(b2Vec2 force)
-        {
-            if (body)
-            {
-                body->ApplyForceToCenter(force, true);
-            }
-        }
-        // Add impulse
-        void AddImpulse(b2Vec2 impulse, b2Vec2 point)
-        {
-            if (body)
-            {
-                body->ApplyLinearImpulse(impulse, point, true);
-            }
-        }
-
-        // Add impulse at the center of mass 
-        void AddImpulseToCenter(b2Vec2 impulse)
-        {
-            if (body)
-            {
-                body->ApplyLinearImpulseToCenter(impulse, true);
-            }
-        }
-        //Add Torque
-        void AddTorque(float torque)
-        {
-            if (body)
-            {
-                body->ApplyTorque(torque, true);
-            }
-        }
-        //Add impulse with torque
-        void AddAngularImpulse(float torque) {
-            if (body) {
-                body->ApplyAngularImpulse(torque, true);
-            }
-        }
+       
         //Set Active
         void SetActive(bool flag) {
             if (body) {
@@ -218,6 +173,7 @@ namespace IonixEngine
         bool GetActive() {
             return body->IsAwake();
         }
+        
         //Rotate Position or Body - doesnt set the angle to be something like Set Angle adds rotation to current body
         void RotatePosition(float angle) {
             float currentAngle = body->GetAngle(); 
