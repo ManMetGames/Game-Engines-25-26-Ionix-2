e0a2ef399c1a11a57048bda806eecd7885c49671
Michalowsk1 <23619282@stu.mmu.ac.uk>
2025-10-21 10:03:33 +0100

    queue implemented
    it is what it is
    
    Co-Authored-By: Tia <146729907+Glowflye@users.noreply.github.com>

--- Engine/src/Graphics/QueueRenderer.cpp
+++ Engine/src/Graphics/QueueRenderer.cpp
@@ -1,9 +1,7 @@
-#include "Graphics/SpriteSorting.h"
+#include "Graphics/QueueRenderer.h"
 
-/*
-Array of sprites which represent z - Level(use array of sprite references, just move pointer not sprite in memory) 
-		- Or use vectors or lists
-		iterate array mutiple times for z level OR sort array by z order and iterate once
-//SDLayer - representation of layers
 
-*/
+void QueueRenderer::AddToQueue()
+{
+
+}
---
+++ Engine/src/Graphics/QueueRenderer.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+#include <string>
+//#include <mutex>
+using namespace std;
+class QueueRenderer //Singleton
+{
+
+private:
+	//member variables
+	queue<string> sprites; //change string type later to sprite/texture
+	//static pointer
+	static QueueRenderer* queueRendPtr;
+
+	//MAYBEEE????? use mutex
+
+public:
+
+	QueueRenderer(const QueueRenderer& obj) = delete; //prevent copis
+	QueueRenderer();
+	void AddToQueue();
+	void RenderFromQueue();
+	void ClearQueue();
+	static QueueRenderer& Get()
+	{
+		static QueueRenderer instance;
+		return instance;
+	}
+
+};
