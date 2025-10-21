015d71b1276ab62934df8a0d035368c4deec9706
JoshuaJ123 <22512619@stu.mmu.ac.uk>
2025-10-21 11:42:42 +0100

    getContact implemented.
    getContact has been implemented for both the box and circle collider using a contact struct. The contact methods determin whether the two objects overlap and create a normal vector which makes it so the two objects no longer overlap. It will then return the contact object so that another method can use it to actually move the two objects apart.

--- Engine/src/Fysics/Collider.h
+++ Engine/src/Fysics/Collider.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "LayerSystem/Layers/LayerFysics.h"
+#include "Maf/MafUtils.h"
 
 namespace IonixEngine
 {
@@ -12,9 +13,16 @@ namespace IonixEngine
             world = LayerFysics::GetInstance()->GetWorld();
         }
 
-        virtual bool isTouching(const Collider& other) const = 0;
+        
+        struct Contact {         //Contact struct
+            bool isTouching;
+            float overlapX;
+            float overlapY;
+            float normalX;
+            float normalY;
+        };
 
-        struct Rect {
+        struct Rect {             //Box struct
             float x, y;           //Origin point (top-left)
             float width, height;
         };
@@ -24,7 +32,50 @@ namespace IonixEngine
             return (a.x < b.x + b.width && a.x + a.width > b.x && a.y < b.y + b.height && a.y + a.height > b.y);
         }
 
-        struct Circle {
+        Contact getContact(const Rect& a, const Rect& b)
+        {
+            //Creates an empty contact and gives variables values
+            Contact c{};
+            float aCentreX = a.x + a.width / 2;
+            float aCentreY = a.y + a.height / 2;
+            float bCentreX = b.x + b.width / 2;
+            float bCentreY = b.y + b.height / 2;
+
+            //Checks for no overlap case
+            float dx = bCentreX - aCentreX;
+            float overlapX = (a.width / 2 + b.width / 2) - Maf::mafAbs(dx);
+            if (overlapX <= 0) return c;
+
+
+            float dy = bCentreY - aCentreY;
+            float overlapY = (a.height / 2 + b.height / 2) - Maf::mafAbs(dy);
+            if (overlapY <= 0) return c;
+
+            //An overlap is detected.
+            c.isTouching = true;
+
+            //Picking the smallest overlap
+            if (overlapX < overlapY)
+            {
+                //Determins the direction of the normal to apply force in the appropriate direction
+                c.overlapX = (dx < 0 ? -overlapX : overlapX);
+                c.overlapY = 0;
+                c.normalX = (dx < 0 ? -1 : 1);
+                c.normalY = 0;
+            }
+            else
+            {
+                //Determins the direction of the normal to apply force in the appropriate direction
+                c.overlapX = 0;
+                c.overlapY = (dy < 0 ? -overlapY : overlapY);
+                c.normalX = 0;
+                c.normalY = (dy < 0 ? -1 : 1);
+            }
+            //Returns the Contact point and the direction of the normal
+            return c;
+        }
+
+        struct Circle {           //Circle struct
             float x, y;           //Centre point
             float radius;
         };
@@ -37,7 +88,36 @@ namespace IonixEngine
             float radiusSum = a.radius + b.radius;
             return (distanceSqr <= radiusSum * radiusSum);
         }
-    };
 
+        Contact getContact(const Circle& a, const Circle& b)
+        {
+            //Creates an empty contact and gives variables values
+            Contact c{};
+            float dx = a.x - b.x;
+            float dy = a.y - b.y;
+            float distanceSqr = dx * dx + dy * dy;
+            float radiusSum = a.radius + b.radius;
+
+            //Checks for no overlap case
+            if (distanceSqr >= radiusSum * radiusSum) return c;
+
+            //An overlap is detected.
+            c.isTouching = true;
+
+            //Determins the true distance between the two circles
+            float dist = Maf::mafSqrt(distanceSqr);
+            //Computes the depth
+            float overlap = radiusSum - dist;
+            //Creates a unit vector for the collision normal
+            c.normalX = dx / dist;
+            c.normalY = dy / dist;
+            //Works out the required distance to move a circle to seperate them
+            c.overlapX = c.normalX * overlap;
+            c.overlapY = c.normalY * overlap;
+
+            //Returns the Contact point and the direction of the normal
+            return c;
+        }
+    };
 }
 
