268bba0d815763cb82516ee7fdba0e67a557e28f
Daniel Cleaver <23665045@stu.mmu.ac.uk>
2025-10-28 14:51:52 +0000

    Sound can now loop and working in lua... Pending testing with headphones

---
+++ Client/Scripts/Audio.lua
@@ -0,0 +1,25 @@
+local AudioSystem = {}
+
+local testSound = nil
+
+function AudioSystem:OnStart()
+    print("[Lua] Audio system initializing...")
+
+    -- Load sound into SoundManager
+    local sm = SoundManager.GetInstance()
+    sm:LoadSound("test", "./Assets/test.wav")
+
+    -- Create AudioPlayer (assuming 'entity' is passed from engine)
+    testSound = AudioPlayer:new(entity, "test", false)
+    testSound.volume = 128
+
+    -- Play sound
+    testSound:Play(0, 3)
+    print("[Lua] Playing test.wav...")
+end
+
+function AudioSystem:OnShutdown()
+    print("[Lua] Audio system shutting down...")
+end
+
+return AudioSystem
