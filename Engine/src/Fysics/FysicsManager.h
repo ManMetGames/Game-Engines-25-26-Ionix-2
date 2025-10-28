29a85651fc3f1bf0969b26cde203b8c3e121c898
Joseph Hill <91340791+Subluxx@users.noreply.github.com>
2025-10-28 11:27:09 +0000

    Bunch'O Voids

--- Engine/src/Fysics/FysicsManager.h
+++ Engine/src/Fysics/FysicsManager.h
@@ -46,5 +46,139 @@ namespace IonixEngine
             joint->setJoint(body->GetBody(), bodyb->GetBody(), b2Vec2_zero, 1.0f, 1.0f, true, 1.0f, 1.0f, true);
 
         }
+
+        void FB_Create()
+        {
+            FysicsBody* body = new FysicsBody(world);
+        }
+
+        void FB_Destroy()
+        {
+
+        }
+
+        void FB_GetPos()
+        {
+
+        }
+
+        void FB_SetPos()
+        {
+
+        }
+
+        void FB_GetAngle()
+        {
+
+        }
+
+        void FB_SetAngle()
+        {
+
+        }
+
+        void FB_GetVel()
+        {
+
+        }
+
+        void FB_SetVel()
+        {
+
+        }
+
+        void FB_GetAngularVel()
+        {
+
+        }
+
+
+        void FB_SetAngularVel()
+        {
+
+        }
+
+        bool FB_IsActive()
+        {
+
+        }
+
+        void FB_SetActive()
+        {
+
+        }
+
+        void FB_GetLinearDampaning()
+        {
+
+        }
+
+        void FB_SetLinearDampaning()
+        {
+
+        }
+
+        void FB_GetAngularDampaning()
+        {
+
+        }
+
+        void FB_SetAngularDampaning()
+        {
+
+        }
+
+        void FB_IsSleepAllowed()
+        {
+
+        }
+
+        void FB_SetSleepAllowed()
+        {
+
+        }
+        
+        void FB_GetFixedRotation()
+        {
+
+        }
+
+        void FB_SetFixedRotation()
+        {
+
+        }
+
+        void FB_IsBullet()
+        {
+
+        }
+
+        void FB_SetIsBullet()
+        {
+
+        }
+
+        void FB_GetGravityScale()
+        {
+
+        }
+
+        void FB_SetGravityScale()
+        {
+
+        }
+       
+
+
+
+
+
+
+        //-------------------------
+        void AddJointToFysicsBody()
+        {
+
+        }
+
     };
 }
