a99c52a97d438063c7d5460730ccc2a94ddad713
Kai Hodgson <146730176+kaihodgson@users.noreply.github.com>
2025-11-01 20:53:00 +0000

    Re-Implemented Binding Files and Refactored Scripting Files

--- Engine/src/Scripting/Scripting.h
+++ Engine/src/Scripting/Scripting.h
@@ -1,48 +1,32 @@
 #pragma once
 #include "sol/sol.hpp"
 #include "Architecture/Scene.h"
-#include "Scripting/Audio/AudioScripting.h"
+
+// Modular binding headers
+#include "Scripting/Bindings/AudioBindings.h"
+#include "Scripting/Bindings/WindowBindings.h"
+#include "Scripting/Bindings/InputBindings.h"
+#include "Scripting/Bindings/MafsBindings.h"
+#include "Scripting/Bindings/GraphicsBindings.h"
+#include "Scripting/Bindings/EntityBindings.h"
+#include "Scripting/Bindings/UIBindings.h"
 
 namespace IonixEngine
 {
     class Scripting
     {
     public:
-        //singleton
         static Scripting& Get();
 
-        //Core
         void Init();
-
-        // Registration Methods
         void RegisterEngineBindings();
-
-        void ExecuteScript(const std::string& scriptName); 
-
+        void ExecuteScript(const std::string& scriptName);
         void CallHook(const std::string& hookName);
 
         sol::state& GetLuaState() { return m_LuaState; }
 
-
     private:
-        //singleton Instance
         static Scripting* s_Instance;
         sol::state m_LuaState;
-
-		void RegisterAudioBindings();
-
-        void RegisterWindowBindings();
-
-        void RegisterInputBindings();
-
-        void RegisterMafsBindings();
-
-        void RegisterGraphicsBindings();
-
-        void RegisterEntityBindings();
-
-        void RegisterUIBindings();
-
     };
 }
-
