f4863e6e8ea03b7706f94d9a463db26fc528931e
Ben <23626950@stu.mmu.ac.uk>
2025-11-04 16:27:45 +0000

    Added methods for calc h value

--- Engine/src/Fysics/NavMef.h
+++ Engine/src/Fysics/NavMef.h
@@ -6,9 +6,17 @@
 #include <iostream>
 
 struct Cell {
-    std::array<int, 3> verts;
+    std::array<int, 4> corns; // index of cell
     std::vector<int> neighbors;
-    float heuristicValue;
+    //float heuristicValue;
+};
+struct Node {
+    int cellIndex;
+    float h;
+    float g;
+    float f;
+    int previousCell;
+
 };
 
 //struct GridCell {
@@ -20,13 +28,13 @@ struct Cell {
 
 class NavMef {
 private:
-    std::vector<b2Vec2> m_vertices;
-    std::vector<Cell> m_cells;
+    std::vector<b2Vec2> m_corners; // corner coords
+    std::vector<Cell> m_cells; // cell
 
     bool ShareEdge(const Cell& a, const Cell& b) {
         int shared = 0;
-        for (int va : a.verts) {
-            for (int vb : b.verts) {
+        for (int va : a.corns) {
+            for (int vb : b.corns) {
                 if (va == vb) {
                     shared++;
                 }
@@ -35,27 +43,27 @@ private:
         }
             
 
-        // if 2 vertices are shared between the triangles, they touch with an edge and are adjacent
+        // if 2 vertices are shared between the rectangles, they touch with an edge and are adjacent
         return shared >= 2;
     }
 
 
 public:
     // load vertices and indices
-    void Load(const std::vector<b2Vec2>& vertices, const std::vector<int>& indices) {
-        m_vertices = vertices;
-        const int threeCount = indices.size() / 3;
-        m_cells.resize(threeCount);
+    void Load(const std::vector<b2Vec2>& corners, const std::vector<int>& indices) {
+        m_corners = corners;
+        const int fourCount = indices.size() / 4;
+        m_cells.resize(fourCount);
 
         // cells
-        for (int i = 0; i < threeCount; i++) {
-            m_cells[i].verts = { indices[i * 3], indices[i * 3 + 1], indices[i * 3 + 2] };
-            m_cells[i].heuristicValue = CalculateHeuristic(m_cells[i].verts);
+        for (int i = 0; i < fourCount; i++) {
+            m_cells[i].corns = { indices[i * 4], indices[i * 4 + 1], indices[i * 4 + 2], indices[i*4+2]}; // the corner values
+            //m_cells[i].heuristicValue = CalculateHeuristic(m_cells);
         }
 
-        // check adjacent triangles with their edges
-        for (int a = 0; a < threeCount; a++) {
-            for (int b = a + 1; b < threeCount; b++) {
+        // check adjacent rectangles with their edges
+        for (int a = 0; a < fourCount; a++) {
+            for (int b = a + 1; b < fourCount; b++) {
                 if (ShareEdge(m_cells[a], m_cells[b])) {
                     m_cells[a].neighbors.push_back(b);
                     m_cells[b].neighbors.push_back(a);
@@ -66,8 +74,22 @@ public:
 
     const std::vector<Cell>& GetCells() const { return m_cells; }
 
-    float CalculateHeuristic(const std::array<int, 3>) {
+    b2Vec2 GetCellCentre(const Cell& cell) const {
+        //finds the centre of the cell (rectangle)  from the 3 verticies;
+        b2Vec2 centre = { 0,0 };
+        for (int cornIndex : cell.corns) {
+            centre += m_corners[cornIndex];
+            
+        }
+        centre *= 0.25f;
+        return centre;
+    }
 
+    float CalculateHeuristic(int cellIndex, int goalIndex) const{
+        //takes current cell index then the goal and calculates the h value by calculating the distance between them
+        b2Vec2 cellCentre = GetCellCentre(m_cells[cellIndex]);
+        b2Vec2 goalCentre = GetCellCentre(m_cells[goalIndex]);
+        return b2Distance(cellCentre, goalCentre);
     }
 
 
