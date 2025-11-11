6a179849b2a38d5247213139a434040c2b9de3e5
Ex-4 <146730152+Ex-4@users.noreply.github.com>
2025-11-11 09:43:38 +0000

    removed the sdl delay (please dont hit me)

--- Engine/src/Graphics/SpriteComponent.cpp
+++ Engine/src/Graphics/SpriteComponent.cpp
@@ -48,7 +48,7 @@ namespace IonixEngine {
 
 		//This is just here so we can see the animation play at a normal speed
 		//THIS WILL BE REMOVED
-		SDL_Delay(60);
+		//SDL_Delay(60);
 
 
 		if ((currentFrame != endFrame) && playbackMode != ONEFRAME)
