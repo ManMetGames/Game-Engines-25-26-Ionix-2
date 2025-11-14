2e9f848d2d4021fd66fd62cbafce57132245cf51
JoshuaJ123 <22512619@stu.mmu.ac.uk>
2025-11-14 13:56:40 +0000

    Issues in Force.h resolved

--- Engine/src/Fysics/Force.h
+++ Engine/src/Fysics/Force.h
@@ -16,24 +16,27 @@ namespace IonixEngine
             body = b;
         }
 
-        void AddForce(b2Vec2 force, b2Vec2 point)
+        void AddForce(Entity* entity, b2Vec2 force, b2Vec2 point)
         {
+            b2Body* body = Application::Get().layerFysics->GetFysicsManager()->GetBodyFromEntity(entity);
             if (body)
             {
                 body->ApplyForce(force, point, true);
             }
         }
 
-        void AddForceToCenter(b2Vec2 force)
+        void AddForceToCenter(Entity* entity, b2Vec2 force)
         {
+            b2Body* body = Application::Get().layerFysics->GetFysicsManager()->GetBodyFromEntity(entity);
             if (body)
             {
                 body->ApplyForceToCenter(force, true);
             }
         }
         // Add impulse
-        void AddImpulse(b2Vec2 impulse, b2Vec2 point)
+        void AddImpulse(Entity* entity, b2Vec2 impulse, b2Vec2 point)
         {
+            b2Body* body = Application::Get().layerFysics->GetFysicsManager()->GetBodyFromEntity(entity);
             if (body)
             {
                 body->ApplyLinearImpulse(impulse, point, true);
@@ -41,8 +44,9 @@ namespace IonixEngine
         }
 
         // Add impulse at the center of mass 
-        void AddImpulseToCenter(b2Vec2 impulse)
+        void AddImpulseToCenter(Entity* entity, b2Vec2 impulse)
         {
+            b2Body* body = Application::Get().layerFysics->GetFysicsManager()->GetBodyFromEntity(entity);
             // add forces to my fysics manager for the body dictionary
             if (body)
             {
@@ -50,23 +54,27 @@ namespace IonixEngine
             }
         }
         //Add Torque
-        void AddTorque(float torque)
+        void AddTorque(Entity* entity, float torque)
         {
+            b2Body* body = Application::Get().layerFysics->GetFysicsManager()->GetBodyFromEntity(entity);
             if (body)
             {
                 body->ApplyTorque(torque, true);
             }
         }
         //Add impulse with torque
-        void AddAngularImpulse(float torque) {
+        void AddAngularImpulse(Entity* entity, float torque) 
+        {
+            b2Body* body = Application::Get().layerFysics->GetFysicsManager()->GetBodyFromEntity(entity);
             if (body) {
                 body->ApplyAngularImpulse(torque, true);
             }
         }
 
         // Clear all forces/velocities on this body
-        void ClearForces()
+        void ClearForces(Entity* entity)
         {
+            b2Body* body = Application::Get().layerFysics->GetFysicsManager()->GetBodyFromEntity(entity);
             if (body)
             {
                 body->SetLinearVelocity(b2Vec2(0.0f, 0.0f));
