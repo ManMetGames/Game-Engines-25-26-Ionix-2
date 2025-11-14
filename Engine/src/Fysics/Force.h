f804f6ecfbcb93bcc86c8af21aa3476507123f55
JoshuaJ123 <22512619@stu.mmu.ac.uk>
2025-11-14 14:32:14 +0000

    Added a Force.cpp file to avoid circular dependancies and moved function definitions over

--- Engine/src/Fysics/Force.h
+++ Engine/src/Fysics/Force.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "LayerSystem/Layers/LayerFysics.h"
+#include "Architecture/Application.h"
 
 namespace IonixEngine
 {
@@ -16,71 +17,16 @@ namespace IonixEngine
             body = b;
         }
 
-        void AddForce(Entity* entity, b2Vec2 force, b2Vec2 point)
-        {
-            b2Body* body = Application::Get().layerFysics->GetFysicsManager()->GetBodyFromEntity(entity);
-            if (body)
-            {
-                body->ApplyForce(force, point, true);
-            }
-        }
+        void AddForce(Entity* entity, b2Vec2 force, b2Vec2 point);
+        void AddForceToCenter(Entity* entity, b2Vec2 force);
 
-        void AddForceToCenter(Entity* entity, b2Vec2 force)
-        {
-            b2Body* body = Application::Get().layerFysics->GetFysicsManager()->GetBodyFromEntity(entity);
-            if (body)
-            {
-                body->ApplyForceToCenter(force, true);
-            }
-        }
-        // Add impulse
-        void AddImpulse(Entity* entity, b2Vec2 impulse, b2Vec2 point)
-        {
-            b2Body* body = Application::Get().layerFysics->GetFysicsManager()->GetBodyFromEntity(entity);
-            if (body)
-            {
-                body->ApplyLinearImpulse(impulse, point, true);
-            }
-        }
+        void AddImpulse(Entity* entity, b2Vec2 impulse, b2Vec2 point);
+        void AddImpulseToCenter(Entity* entity, b2Vec2 impulse);
+        void AddAngularImpulse(Entity* entity, float torque);
 
-        // Add impulse at the center of mass 
-        void AddImpulseToCenter(Entity* entity, b2Vec2 impulse)
-        {
-            b2Body* body = Application::Get().layerFysics->GetFysicsManager()->GetBodyFromEntity(entity);
-            // add forces to my fysics manager for the body dictionary
-            if (body)
-            {
-                body->ApplyLinearImpulseToCenter(impulse, true);
-            }
-        }
-        //Add Torque
-        void AddTorque(Entity* entity, float torque)
-        {
-            b2Body* body = Application::Get().layerFysics->GetFysicsManager()->GetBodyFromEntity(entity);
-            if (body)
-            {
-                body->ApplyTorque(torque, true);
-            }
-        }
-        //Add impulse with torque
-        void AddAngularImpulse(Entity* entity, float torque) 
-        {
-            b2Body* body = Application::Get().layerFysics->GetFysicsManager()->GetBodyFromEntity(entity);
-            if (body) {
-                body->ApplyAngularImpulse(torque, true);
-            }
-        }
+        void AddTorque(Entity* entity, float torque);
 
-        // Clear all forces/velocities on this body
-        void ClearForces(Entity* entity)
-        {
-            b2Body* body = Application::Get().layerFysics->GetFysicsManager()->GetBodyFromEntity(entity);
-            if (body)
-            {
-                body->SetLinearVelocity(b2Vec2(0.0f, 0.0f));
-                body->SetAngularVelocity(0.0f);
-            }
-        }
+        void ClearForces(Entity* entity);
         
     };
     
