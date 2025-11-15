6c93b62d83ab69e04c27e98d35dd7d3a68f7f0dc
LucasCarter-8 <23636298@stu.mmu.ac.uk>
2025-11-15 15:57:39 +0000

    Scripting - Fysics angle, velocity and awake methods

--- Engine/src/Scripting/Fysics/FysicsScripting.cpp
+++ Engine/src/Scripting/Fysics/FysicsScripting.cpp
@@ -18,8 +18,72 @@ namespace IonixEngine
 			};
 
 		auto setFysicsPos = [](Entity* entity, float x, float y) {
-			return entity->GetComponent<FysicsBody>()->SetPosition(entity, x, y);
+			entity->GetComponent<FysicsBody>()->SetPosition(entity, x, y);
 			};
+
+		auto getFysicsAngle = [](Entity* entity) -> float{
+			return entity->GetComponent<FysicsBody>()->GetAngle(entity);
+			};
+
+		auto setFysicsAngle = [](Entity* entity, float radians) {
+			entity->GetComponent<FysicsBody>()->SetAngle(entity, radians);
+			};
+
+		auto getFysicsLinearVelocity = [](Entity* entity) -> b2Vec2{
+			return entity->GetComponent<FysicsBody>()->GetLinearVelocity(entity);
+			};
+
+		auto setFysicsLinearVelocity = [](Entity* entity, float x, float y) {
+			entity->GetComponent<FysicsBody>()->SetLinearVelocity(entity, x, y);
+			};
+
+		auto getFysicsAngularVelocity = [](Entity* entity) -> float {
+			return entity->GetComponent<FysicsBody>()->GetAngularVelocity(entity);
+			};
+
+		auto setFysicsAngularVelocity = [](Entity* entity, float x) {
+			entity->GetComponent<FysicsBody>()->SetAngularVelocity(entity, x);
+			};
+
+		auto getFysicsAwake = [](Entity* entity) -> bool {
+			return entity->GetComponent<FysicsBody>()->GetAwake(entity);
+			};
+
+		auto setFysicsAwake = [](Entity* entity, bool flag) {
+			entity->GetComponent<FysicsBody>()->SetAwake(entity, flag);
+			};
+
+		auto rotateFysicsPosition = [](Entity* entity, float angle) {
+			entity->GetComponent<FysicsBody>()->RotatePosition(entity, angle);
+			};
+
+		auto getFysicsLinearDamping = [](Entity* entity) -> float{
+			return entity->GetComponent<FysicsBody>()->GetLinearDamping(entity);
+			};
+
+		auto setFysicsLinearDamping = [](Entity* entity, float damping) {
+			entity->GetComponent<FysicsBody>()->SetLinearDamping(entity, damping);
+			};
+
+		auto getFysicsAngularDamping = [](Entity* entity) -> float{
+			return entity->GetComponent<FysicsBody>()->GetAngularDamping(entity);
+			};
+
+		auto setFysicsAngularDamping = [](Entity* entity, float damping) {
+			entity->GetComponent<FysicsBody>()->SetAngularDamping(entity, damping);
+			};
+
+		auto getFysicsAllowSleep = [](Entity* entity) -> bool {
+			return entity->GetComponent<FysicsBody>()->GetAllowSleep(entity);
+			};
+
+		auto setFysicsAllowSleep = [](Entity* entity, bool flag) {
+			entity->GetComponent<FysicsBody>()->SetAllowSleep(entity, flag);
+			};
+
+
+
+
 	}
 }
 
