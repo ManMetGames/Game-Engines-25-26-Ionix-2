bcede5953947ed4c3f2fe6f30fd73d644a018686
VSky26 <varunamarnani26@gmail.com>
2025-11-18 14:32:37 +0000

    Added Images

--- Engine/src/Architecture/Assets.hpp
+++ Engine/src/Architecture/Assets.hpp
@@ -3,6 +3,7 @@
 #include <string>
 
 struct Textures {
+    uint64_t Background = 13067482129777558234u;
     uint64_t Debug = 16105671890965812228u;
     uint64_t FlappyBird = 12156800037729862389u;
     uint64_t Sand = 4972914212072997432u;
@@ -13,6 +14,7 @@ struct Textures {
 
     inline std::map<std::string, std::string> GetTextures() {
         std::map<std::string, std::string> map;
+        map["Background"] = "./Assets/Background.png";
         map["Debug"] = "./Assets/Debug.png";
         map["FlappyBird"] = "./Assets/FlappyBird.png";
         map["Sand"] = "./Assets/Sand.png";
