ae7bea36d6dab9f80b2bb6eef633b49152fe9b75
Zul <146729926+Zulfaqaar@users.noreply.github.com>
2025-11-18 15:22:34 +0000

    Manually merged UI main
    - Changes made to the UI-Main branch hadn't been merged into main.

--- Engine/src/UI/UIManager.h
+++ Engine/src/UI/UIManager.h
@@ -3,6 +3,9 @@
 #include <string>
 #include <functional>
 #include <vector>
+#include "Fontloader.h"
+
+#include "UI/UI.h"
 namespace IonixEngine
 {
 	enum UIType
@@ -16,11 +19,12 @@ namespace IonixEngine
 		RadioButton,
 		Dropdown,
 		ColorPicker,
+		ProgressBar,
 	};
 	struct UIElement
 	{
 		UIType type;
-		//std::string groupName;
+		std::string groupName;
 		int xPos;
 		int yPos;
 		float xSize;
@@ -37,62 +41,76 @@ namespace IonixEngine
 		int RadioButtonValue = 0;
 		bool sameline = false;
 		float* color = nullptr; // only for ColorPicker4
-		
+
+		// Font name for this element
+		std::string fontName;
+
+		float maxValue = 0.0f; // only for ProgressBar
+		float* currentValue = nullptr; // only for ProgressBar
+		float incrementAmount = 0.0f; // only for ProgressBar
 		std::vector<UIElement> children;
 		bool isChildGroup = false;
 
 		std::vector<std::string> dropdownOptions;
 		int* dropdownCurrentIndex = nullptr;
-		
+
 	};
 
 	class UIManager
 	{
-	private: 
+	private:
 		//std::string currentGroupName; 
 
 		//std::vector<UIElement*> groupStack; 
 
 		void RenderElement(UIElement& element);
-	public:
-		void BeginGroup(const std::string& groupName);
 
+		UI* m_ui = nullptr;
+	public:
+		Fontloader fontLoader;
 		std::vector<UIElement> GetElements()
 		{
 			return elements;
 		}
 
 		std::vector<UIElement> elements;
-		
-		//void EndGroup();
+		void setUIRenderer(UI* ui)
+		{
+			m_ui = ui;
+		}
+		//void UIManager::BeginGroup(const std::string& groupName);
 
-		void BeginPanel(const std::string& panelName);
+		//void UIManager::EndGroup();
 
-		void EndPanel();
+		void UIManager::BeginPanel(const std::string& panelName);
 
-		void AddChildToPanel(UIElement element);
-		
+		void UIManager::EndPanel();
+
+		void UIManager::AddChildToPanel(UIElement element);
+
 		// Add for new UITypes below
-		void AddLabel(int x, int y, float xSize, float ySize, const char* text);
-		
-		void AddButton(int x, int y, float xSize, float ySize, const char* text, std::function<void()> onClick);
-		
-		void AddCheckbox(int x, int y, float xSize, float ySize, const char* text, bool* checked);
-		
-		void AddSliderFloat(int x, int y, float xSize, float ySize, const char* text, float* value, float min, float max);
-		
-		void AddInputText(int x, int y, float xSize, float ySize, const char* text, char* buffer, size_t bufferSize);
-		
-		void AddRadioButton(int x, int y, float xSize, float ySize, const char* text, int* radioValuePointer, int value, bool sameline);
-		
-		void AddDropdown(int x, int y, float xSize, float ySize, const char* text, std::vector<std::string> options, int* currentIndex);
-		
-		void AddColorPicker(int x, int y, float xSize, float ySize, const char* label, float* color);
+
+		void AddLabel(int x, int y, float xSize, float ySize, const char* text, const std::string& fontName = "");
+
+		void AddButton(int x, int y, float xSize, float ySize, const char* text, std::function<void()> onClick, const std::string& fontName = "");
+
+		void AddCheckbox(int x, int y, float xSize, float ySize, const char* text, bool* checked, const std::string& fontName = "");
+
+		void AddSliderFloat(int x, int y, float xSize, float ySize, const char* text, float* value, float min, float max, const std::string& fontName = "");
+
+		void AddInputText(int x, int y, float xSize, float ySize, const char* text, char* buffer, size_t bufferSize, const std::string& fontName = "");
+
+		void AddRadioButton(int x, int y, float xSize, float ySize, const char* text, int* radioValuePointer, int value, bool sameline = false, const std::string& fontName = "");
+
+		void AddDropdown(int x, int y, float xSize, float ySize, const char* text, std::vector<std::string> options, int* currentIndex, const std::string& fontName = "");
+
+		void AddColorPicker(int x, int y, float xSize, float ySize, const char* label, float* color, const std::string& fontName = "");
 
 		void ClearElements();
 
+		void AddProgressBar(int x, int y, float xSize, float ySize, float maxvalue, float* currentvalue, float incrementamount, const std::string& fontName = "");
+
 		void RenderUI();
-		
+
 	};
 }
-
