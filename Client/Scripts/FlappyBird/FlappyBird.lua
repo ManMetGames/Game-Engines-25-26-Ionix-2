8b7a5ce05bbd860e5950f883291ce72f1ed0b14d
Xandru175 <xandrupuiu@gmail.com>
2025-11-25 10:41:10 +0000

    Automatic Sprite Scaling

--- Client/Scripts/FlappyBird/FlappyBird.lua
+++ Client/Scripts/FlappyBird/FlappyBird.lua
@@ -10,6 +10,12 @@ local goalY = 500
 local y = 300
 local t = 10
 
+-- Pipe variables
+local pipe
+local pipeSpeed = -3
+local pipeStartX = 900
+local pipeOffScreenLeft = -100
+
 ----------------------------------------------------------
 -- OnStart
 ----------------------------------------------------------
@@ -25,12 +31,13 @@ function ExampleScript:OnStart()
 	Texture.add_texture("./Assets/key.png", "key")
     Texture.add_texture("./Assets/FlappyBird.png", "FlappyBird")
     Texture.add_texture("./Assets/Background.png", "Background")
+    Texture.add_texture("./Assets/FlappyPipe.png", "FlappyPipe")
     ------------------------------------------------------
     -- Create player1
     ------------------------------------------------------
     player1 = Entity.create_entity()
 
-    Entity.set_entity_pos(player1, x, 300)
+    Entity.set_entity_pos(player1, x, 500)
 	
     local playerSprite1 = Entity.add_sprite_component(player1, "FlappyBird", 64, 64, 0)
 
@@ -69,6 +76,21 @@ function ExampleScript:OnStart()
 		Entity.add_fysics_component(tile, 0, false)  -- static
 		Fysics.add_sprite_collider(tile, false)
 	end
+
+	------------------------------------------------------
+	-- Create pipe obstacle
+	------------------------------------------------------
+	pipe = Entity.create_entity()
+	Entity.set_entity_pos(pipe, 400, 400)
+
+	local pipeSprite = Entity.add_sprite_component(pipe, "BottomPipe", 0, 0, 0)
+
+    pipeSprite.set_width(480)
+	Sprite.set_playback_mode(pipeSprite, 4)
+
+	-- Kinematic body so it moves but isn't affected by gravity
+	Entity.add_fysics_component(pipe, 1, false)
+	Fysics.add_sprite_collider(pipe, false)
 end
 
 ----------------------------------------------------------
@@ -92,6 +114,13 @@ function ExampleScript:OnUpdate()
 	end
 
     Fysics.set_linear_velocity(player1, vx, vy1)
+
+    -- Pipe movement disabled for testing
+    -- Fysics.set_linear_velocity(pipe, pipeSpeed, 0)
+    -- local pipePos = Fysics.get_pos(pipe)
+    -- if pipePos.x < pipeOffScreenLeft then
+    --     Fysics.set_pos(pipe, pipeStartX, pipePos.y)
+    -- end
 end
 
 return ExampleScript
