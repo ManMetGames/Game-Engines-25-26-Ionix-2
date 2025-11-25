65875842fc67df291285aa74f49d4aeb840fec7f
bentatam <146729919+bentatam@users.noreply.github.com>
2025-11-25 15:57:03 +0000

    Squashed commit of the following:
    commit b58fd2469037543ed54eebf3c2b0b24564d4a2ff
    Author: Wendy Yu <146729946+Wyu03@users.noreply.github.com>
    Date:   Tue Nov 25 15:39:10 2025 +0000
    
        Pipe colliders update
    
        Co-Authored-By: Zul <146729926+Zulfaqaar@users.noreply.github.com>
        Co-Authored-By: VSky26 <183742499+VSky26@users.noreply.github.com>
        Co-Authored-By: Adam Faraj <76076640+AKorty7@users.noreply.github.com>
        Co-Authored-By: Alex Brooke <36033849+Brookey1201@users.noreply.github.com>
    
    commit 424bf4c651d2f2059e736d98e9bb13533e83ac22
    Author: Wendy Yu <146729946+Wyu03@users.noreply.github.com>
    Date:   Tue Nov 25 15:26:55 2025 +0000
    
        gitignore update
    
        Co-Authored-By: Zul <146729926+Zulfaqaar@users.noreply.github.com>
        Co-Authored-By: VSky26 <183742499+VSky26@users.noreply.github.com>
        Co-Authored-By: Adam Faraj <76076640+AKorty7@users.noreply.github.com>
        Co-Authored-By: Alex Brooke <36033849+Brookey1201@users.noreply.github.com>
    
    commit f49cf023967587bf6c24ef2b1b495dd5156b68ac
    Author: Wendy Yu <146729946+Wyu03@users.noreply.github.com>
    Date:   Tue Nov 25 15:25:23 2025 +0000
    
        Pipe update
    
        Co-Authored-By: Zul <146729926+Zulfaqaar@users.noreply.github.com>
        Co-Authored-By: VSky26 <183742499+VSky26@users.noreply.github.com>
        Co-Authored-By: Adam Faraj <76076640+AKorty7@users.noreply.github.com>
        Co-Authored-By: Alex Brooke <36033849+Brookey1201@users.noreply.github.com>
    
    commit ba6f4e6c80bacdfaa179234786f8c5635e2fd56d
    Author: Wendy Yu <146729946+Wyu03@users.noreply.github.com>
    Date:   Tue Nov 25 15:04:49 2025 +0000
    
        Start bird and pipe when key is pressed
    
        Co-Authored-By: Zul <146729926+Zulfaqaar@users.noreply.github.com>
        Co-Authored-By: VSky26 <183742499+VSky26@users.noreply.github.com>
        Co-Authored-By: Adam Faraj <76076640+AKorty7@users.noreply.github.com>
        Co-Authored-By: Alex Brooke <36033849+Brookey1201@users.noreply.github.com>
    
    commit 170d113cb9fb29e319082718c4d589bab571b57d
    Author: Wendy Yu <146729946+Wyu03@users.noreply.github.com>
    Date:   Tue Nov 25 14:58:17 2025 +0000
    
        Pipe Now Moving
    
        Co-Authored-By: Zul <146729926+Zulfaqaar@users.noreply.github.com>
        Co-Authored-By: VSky26 <183742499+VSky26@users.noreply.github.com>
        Co-Authored-By: Adam Faraj <76076640+AKorty7@users.noreply.github.com>
        Co-Authored-By: Alex Brooke <36033849+Brookey1201@users.noreply.github.com>
    
    commit 1b0bd76f7955fb5c7c2788e060d45e506801c0a1
    Author: Wendy Yu <146729946+Wyu03@users.noreply.github.com>
    Date:   Tue Nov 25 14:38:37 2025 +0000
    
        Fixed Pipe size
    
    commit ec12b548291729a0049468af232e9135c98cadc2
    Author: Wendy Yu <146729946+Wyu03@users.noreply.github.com>
    Date:   Tue Nov 25 11:51:27 2025 +0000
    
        Rendered Pipe
    
        Co-Authored-By: Zul <146729926+Zulfaqaar@users.noreply.github.com>
        Co-Authored-By: VSky26 <183742499+VSky26@users.noreply.github.com>
    
    commit d2b6773bf9e7df5355b7bd360e626f908dda5999
    Author: Wendy Yu <146729946+Wyu03@users.noreply.github.com>
    Date:   Tue Nov 25 11:31:13 2025 +0000
    
        Remove unused variables
    
        Co-Authored-By: Zul <146729926+Zulfaqaar@users.noreply.github.com>
        Co-Authored-By: VSky26 <183742499+VSky26@users.noreply.github.com>
    
    commit 192cacabcfb6f3849c7d3555cd8531aa8ff8e5a5
    Merge: d50c8fc 8b7a5ce
    Author: Wendy Yu <146729946+Wyu03@users.noreply.github.com>
    Date:   Tue Nov 25 11:09:28 2025 +0000
    
        merge update
    
    commit d50c8fccb9e794cc459f050818c120d46a23bb49
    Author: Wendy Yu <146729946+Wyu03@users.noreply.github.com>
    Date:   Tue Nov 25 10:52:36 2025 +0000
    
        Set Bird Gravity/state
    
        Co-Authored-By: Zul <146729926+Zulfaqaar@users.noreply.github.com>
        Co-Authored-By: VSky26 <183742499+VSky26@users.noreply.github.com>
        Co-Authored-By: Adam Faraj <76076640+AKorty7@users.noreply.github.com>
        Co-Authored-By: Alex Brooke <36033849+Brookey1201@users.noreply.github.com>

--- Client/Scripts/FlappyBird/FlappyBird.lua
+++ Client/Scripts/FlappyBird/FlappyBird.lua
@@ -9,7 +9,13 @@ local goalX = 500
 local goalY = 500
 local y = 300
 local t = 10
-local coin
+
+-- Pipe variables
+local pipe
+local pipeT
+local pipeSpeed = -3
+local pipeStartX = 900
+local pipeOffScreenLeft = -100
 
 ----------------------------------------------------------
 -- OnStart
@@ -26,50 +32,40 @@ function ExampleScript:OnStart()
 	Texture.add_texture("./Assets/right.png", "right")
 	Texture.add_texture("./Assets/player1.png", "player1")
 	Texture.add_texture("./Assets/key.png", "key")
-    Texture.add_texture("./Assets/Coin", "Coin")
+    Texture.add_texture("./Assets/FlappyPipe.png", "FlappyPipe")
+    Texture.add_texture("./Assets/FlappyPipe2.png", "FlappyPipe2")
 
     ------------------------------------------------------
 	-- Background Texture
 	------------------------------------------------------
     Background = Entity.create_entity()
-    local BgBackground = Entity.add_sprite_component(Background, "Background", 960,640, 0)
+    local BgBackground = Entity.add_sprite_component(Background, "Background", 960, 640, 0)
     Sprite.set_width(BgBackground, 1280)
     Sprite.set_height(BgBackground, 1280)
     Sprite.set_playback_mode(BgBackground, 4)
+
     ------------------------------------------------------
     -- Create player1
     ------------------------------------------------------
     player1 = Entity.create_entity()
+
     Entity.set_entity_pos(player1, x, 300)
-    local playerSprite1 = Entity.add_sprite_component(player1, "FlappyBird", 100, 100, 0)
-    Sprite.set_width(playerSprite1, 64)
-    Sprite.set_height(playerSprite1, 64)
+	
+    local playerSprite1 = Entity.add_sprite_component(player1, "FlappyBird", 0, 0, 0)
+
 	Sprite.set_playback_mode(playerSprite1, 4)
 
     -- PLAYER 1 PHYSICS
+
     Entity.add_fysics_component(player1, 2, false) -- dynamic body
-    Fysics.add_sprite_collider(player1, false, 0.5)
+    --Fysics.add_sprite_collider(player1, false)
+    Fysics.add_box_collider(player1, 1, 1, 0, 0, 0, false)
+    -- Freeze bird
     Fysics.set_gravity_scale(player1, 0)
+
+
     local tileSize = 64
     local floorY = 600
-
-    ------------------------------------------------------
-    -- Create Coins 
-    ------------------------------------------------------
-    coin = Entity.create_entity()
-    Entity.set_entity_pos(coin, x, 300)
-
-    local coinSprite = Entity.add_sprite_component(coin, "Coin", 80, 16, 0)
-    Sprite.set_width(coinSprite, 80)
-    Sprite.set_height(coinSprite, 16)
-	Sprite.set_playback_mode(coinSprite, 4)
-    ------------------------------------------------------
-	-- add physics body + collider
-	------------------------------------------------------
-	Entity.add_fysics_component(coin, 1, false)  -- static
-    Fysics.add_sprite_collider(coin, false)
-
-
 	------------------------------------------------------
 	-- pick texture for left / middle / right
 	------------------------------------------------------
@@ -95,6 +91,39 @@ function ExampleScript:OnStart()
 		Fysics.add_sprite_collider(tile, false)
 	end
 
+	------------------------------------------------------
+	-- Create pipe obstacle
+	------------------------------------------------------
+    --BOTTOM PIPE
+	pipe = Entity.create_entity()
+	Entity.set_entity_pos(pipe, 640, 400)
+
+	local pipeSprite = Entity.add_sprite_component(pipe, "FlappyPipe", 80, 185, 0)
+    Sprite.set_width(pipeSprite, 480)
+    Sprite.set_height(pipeSprite, 1845)
+	Sprite.set_playback_mode(pipeSprite, 4)
+
+	-- Kinematic body so it moves but isn't affected by gravity
+	Entity.add_fysics_component(pipe, 1, false)
+	Fysics.add_sprite_collider(pipe, false)
+
+
+    -- TOP PIPE
+    pipeT = Entity.create_entity()
+	Entity.set_entity_pos(pipeT, 640, 0)
+
+	local pipeSpriteT = Entity.add_sprite_component(pipeT, "FlappyPipe2", 80, 185, 0)
+    Sprite.set_width(pipeSpriteT, 480)
+    Sprite.set_height(pipeSpriteT, 1845)
+	Sprite.set_playback_mode(pipeSpriteT, 4)
+
+	-- Kinematic body so it moves but isn't affected by gravity
+	Entity.add_fysics_component(pipeT, 1, false)
+	Fysics.add_sprite_collider(pipeT, false)
+
+    if Input.get_key_down(Keys.ionix_a) then
+        Entity.set_entity_pos(pipe, xPos, floorY)
+	end
 end
 
 ----------------------------------------------------------
@@ -103,22 +132,31 @@ end
 function ExampleScript:OnUpdate()
     -- get current velocity
     local vel1 = Fysics.get_linear_velocity(player1)
-    
+    local vy1 = Fysics.get_linear_velocity(pipe)
+    local vy1 = Fysics.get_linear_velocity(pipeT)
     -- Constant rightward movement
     local vx = 0
     local vy1 = vel1.y
 
 	if Input.get_key_down(Keys.ionix_space) then
+        -- Bird move if space is pressed (allow gravity)
         Fysics.set_gravity_scale(player1, 1)
+        Fysics.set_linear_velocity(pipe, pipeSpeed, 0)
+        Fysics.set_linear_velocity(pipeT, pipeSpeed, 0)
+        -- Set velocity directly to cancel out falling momentum
         vy1 = -5  -- Jump velocity for player1
-	end
-	
-	if Input.get_key_down(Keys.ionix_a) then
-        Entity.set_entity_pos(player1, xPos, floorY)
 	end
 
     Fysics.set_linear_velocity(player1, vx, vy1)
-    --UI.draw_label("Press any key to play", 20, 20, 300, 300, "Bold")
+
+    -- Pipe movement
+    local pipePos = Fysics.get_pos(pipe)
+    local pipePos = Fysics.get_pos(pipeT)
+    if pipePos.x < pipeOffScreenLeft then
+        Fysics.set_pos(pipe, pipeStartX, pipePos.y)
+        Fysics.set_pos(pipeT, pipeStartX, pipePos.y)
+     end
+     
 end
 
 return ExampleScript
