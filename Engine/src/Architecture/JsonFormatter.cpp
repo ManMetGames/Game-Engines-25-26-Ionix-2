25e5ebcf749f97edc4963dd2ad0c9bafab076c9d
PhantomRonix <157494091+PhantomRonix@users.noreply.github.com>
2025-11-25 11:41:03 +0000

    Feature: Basic File Reading (GAME-509)
    JsonFormatter is now capable of basic file read operations and records filedata to a char vector.

--- Engine/src/Architecture/JsonFormatter.cpp
+++ Engine/src/Architecture/JsonFormatter.cpp
@@ -1,1 +1,56 @@
 #include "JsonFormatter.h"
+#include <iostream>
+#include <fstream>
+#include <string_view>
+
+namespace IonixEngine
+{
+	JsonFormatter::JsonFormatter(const std::string& filepath)
+	{
+		this->filepath = filepath;
+		if (!OpenFile())
+		{
+			std::cout << "[JSON Formatter] Failed to find file!\nRun OpenFile() with a valid filepath." << std::endl;
+		}
+	}
+
+	bool JsonFormatter::OpenFile()
+	{
+		std::ifstream jsonFile;
+		jsonFile.open(filepath);
+		if (jsonFile.is_open())
+		{
+			char character;
+			while (jsonFile.get(character))
+			{
+				fileContents.push_back(character);
+			}
+			return true;
+		}
+		else { return false; }
+	}
+
+	std::string JsonFormatter::GetFilepath()
+	{
+		return filepath;
+	}
+
+	void JsonFormatter::SetFilepath(const std::string& newFilepath)
+	{
+		filepath = newFilepath;
+		OpenFile();
+	}
+
+	void JsonFormatter::DebugLogFileContents()
+	{
+		std::cout << "[JSON Formatter] Logging file contents:" << std::endl;
+		for (char character : fileContents)
+		{
+			std::cout << character;
+		}
+		std::cout << std::endl;
+	}
+
+
+
+}
--- Engine/src/Architecture/JsonFormatter.h
+++ Engine/src/Architecture/JsonFormatter.h
@@ -1,12 +1,24 @@
 #include <string>
 #include <vector>
-#include <fstream>
 #include <map>
 #pragma once
 
-//namespace ionixEngine
-//{
+namespace IonixEngine
+{
 
+class JsonFormatter
+{
+private:
+	std::vector<char> fileContents;
+	std::string filepath;
 
+public:
+	JsonFormatter(const std::string& filepath);
+	bool OpenFile();
 
-//}
+	std::string GetFilepath();
+	void SetFilepath(const std::string& newFilepath);
+
+	void DebugLogFileContents();
+};
+}
