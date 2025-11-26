e6dbed15692d7def6273cc493ac09d9563ff4afa
Sounded <146730001+roberts-pe@users.noreply.github.com>
2025-11-26 12:26:05 +0000

    Update: Readded ball.lua to setting.lua

--- Client/Scripts/Settings.lua
+++ Client/Scripts/Settings.lua
@@ -10,8 +10,9 @@ end
 --LoadModule("Audio", "Scripts/Audio.lua")
 --LoadModule("Enemy", "Scripts/Enemy.lua")
 --LoadModule("Player", "Scripts/Player.lua")
-LoadModule("FlappyBird", "Scripts/FlappyBird/FlappyBird.lua")
+--LoadModule("FlappyBird", "Scripts/FlappyBird/FlappyBird.lua")
 --LoadModule("ExampleScript", "Scripts/ExampleScript.lua")
+--LoadModule("Ball","Scripts/Ball.lua")
 
 -- Lifecycle hooks
 function OnStart()
