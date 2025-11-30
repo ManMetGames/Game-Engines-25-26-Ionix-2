ce2b773a2cae330e570017f356b42f763c4c74a4
JoshCoulbeck <23656773@stu.mmu.ac.uk>
2025-11-30 14:49:14 +0000

    Made simple movement left and right and up

--- Client/Scripts/Snake-Demo/Snake-Demo.lua
+++ Client/Scripts/Snake-Demo/Snake-Demo.lua
@@ -12,12 +12,6 @@ local y = 300
 local t = 10
 
 
--- Pipe variables
-local pipe
-local pipeT
-local pipeSpeed = -3
-local pipeStartX = 900
-local pipeOffScreenLeft = -100
 
 ----------------------------------------------------------
 -- OnStart
@@ -38,10 +32,10 @@ local BgBackground = Entity.add_sprite_component(Background, assets.textures.Bac
     -- PLAYER 1 PHYSICS
 
     Entity.add_fysics_component(player1, 2, true) -- dynamic body
-    --Fysics.add_sprite_collider(player1, false)
+    Fysics.add_sprite_collider(player1, false)
     Fysics.add_sprite_collider(player1,false,1)
     -- Freeze bird
-    Fysics.set_gravity_scale(player1, 0) 
+    
 
       local tileSize = 64
     local floorY = 600
@@ -68,34 +62,11 @@ local BgBackground = Entity.add_sprite_component(Background, assets.textures.Bac
 
 end
 
-------------------------------------------------------
-	-- Create pipe obstacle
-	------------------------------------------------------
-    --BOTTOM PIPE
-	pipe = Entity.create_entity()
-	Entity.set_entity_pos(pipe, 640, 400)
-
-	local pipeSprite = Entity.add_sprite_component(pipe, assets.textures.FlappyPipe, 80, 300, 0)
-    Sprite.set_columns(pipeSprite,1)
-	-- Kinematic body so it moves but isn't affected by gravity
-	Entity.add_fysics_component(pipe, 1, false)
-	Fysics.add_sprite_collider(pipe, false,1)
-
-
-    -- TOP PIPE
-    pipeT = Entity.create_entity()
-	Entity.set_entity_pos(pipeT, 640, 0)
-
-	local pipeSpriteT = Entity.add_sprite_component(pipeT,assets.textures.FlappyPipe , 80, 300, 0)
-    Sprite.set_columns(pipeSpriteT,1)
-	-- Kinematic body so it moves but isn't affected by gravity
-	Entity.add_fysics_component(pipeT, 1, false)
-	Fysics.add_sprite_collider(pipeT, false,1)
-
-    if Input.get_key_down(Keys.ionix_a) then
-        Entity.set_entity_pos(pipe, xPos, floorY)
-	end
 end
+  
+    
+	
+
 
 ----------------------------------------------------------
 -- OnUpdate
@@ -104,36 +75,32 @@ function ExampleScript:OnUpdate()
 
  -- get current velocity
     local vel1 = Fysics.get_linear_velocity(player1)
-    local vy1 = Fysics.get_linear_velocity(pipe)
-    local vy1 = Fysics.get_linear_velocity(pipeT)
+   
     -- Constant rightward movement
     local vx = 0
     local vy1 = vel1.y
 
 	if Input.get_key_down(Keys.ionix_space) then
         -- Bird move if space is pressed (allow gravity)
-        Fysics.set_gravity_scale(player1, 1)
-        Fysics.set_linear_velocity(pipe, pipeSpeed, 0)
-        Fysics.set_linear_velocity(pipeT, pipeSpeed, 0)
+
+      
         -- Set velocity directly to cancel out falling momentum
         vy1 = -5  -- Jump velocity for player1
-  
-    if Input.get_key_held(Keys.ionix_w) then
+  end
+    if Input.get_key_held(Keys.ionix_a) then
+       
+        vx = -5
+    end        
+
+ if Input.get_key_held(Keys.ionix_d) then
        
         vx = 5
     end        
 
-end
 
  Fysics.set_linear_velocity(player1, vx, vy1)
 
-   -- Pipe movement
-    local pipePos = Fysics.get_pos(pipe)
-    local pipePos = Fysics.get_pos(pipeT)
-    if pipePos.x < pipeOffScreenLeft then
-        Fysics.set_pos(pipe, pipeStartX, pipePos.y)
-        Fysics.set_pos(pipeT, pipeStartX, pipePos.y)
-     end
+  
       
 end
 
