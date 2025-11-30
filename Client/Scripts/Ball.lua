0c53faabfed4d579728c0ee8c1b325e9a70e5dc8
Sounded <146730001+roberts-pe@users.noreply.github.com>
2025-11-30 17:16:37 +0000

    Update Ball.lua

--- Client/Scripts/Ball.lua
+++ Client/Scripts/Ball.lua
@@ -3,16 +3,16 @@ local assets = require("Scripts.Assets")
 
 local ballEntity
 local Background
-local speedX = 6
-local speedY = 6
+local speedX = 0.001
+local speedY = 0.001
 local ballSize = 32
 local screenW
 local screenH
 
 function ballInit()
     ballEntity = Entity.create_entity()
-    Entity.set_entity_pos(ballEntity, screenW / 2, screenH / 2)
-    Entity.add_sprite_component(ballEntity, assets.textures.PimBall, 0, 0, 100)
+    Entity.set_entity_pos(ballEntity, screenW / 4, screenH / 4)
+    Entity.add_sprite_component(ballEntity, assets.textures.PimBall, 64, 64, 100)
     Entity.add_fysics_component(ballEntity, 2, false)
     Fysics.add_sprite_collider(ballEntity, false)
    
@@ -20,7 +20,7 @@ end
 
 function BackgroundInnit()
     Background = Entity.create_entity()
-    Entity.add_sprite_component(Background, assets.textures.Background, 0, 0, 0)
+    Entity.add_sprite_component(Background, assets.textures.office, 960, 640, 0)
 end
 function WallInnit()
     local floor = Entity.create_entity()
@@ -54,9 +54,10 @@ end
 
 function ball:OnUpdate()
     local vel1 = Fysics.get_linear_velocity(ballEntity)
-    local vx = 0
+    local vx = vel1.x
     local vy1 = vel1.y
     Fysics.set_linear_velocity(ballEntity, vx, vy1)
+    print(Entity.get_entity_pos(ballEntity).x,Entity.get_entity_pos(ballEntity).y)
 end
 
 return ball
