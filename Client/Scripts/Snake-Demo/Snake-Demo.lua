2de1c552d2985e307cc24adedd9735ec6667197a
JoshCoulbeck <23656773@stu.mmu.ac.uk>
2025-12-02 14:57:54 +0000

    Added new snake stuff to main branch
    Co-Authored-By: EmmettCoyle <146730021+EmmettCoyle@users.noreply.github.com>

--- Client/Scripts/Snake-Demo/Snake-Demo.lua
+++ Client/Scripts/Snake-Demo/Snake-Demo.lua
@@ -10,7 +10,14 @@ local goalX = 500
 local goalY = 500
 local y = 300
 local t = 10
-
+local h = 500
+local w = 400
+local text = "Hello, World!"
+local font = assets.fonts.DefaultFont
+local Apple
+local AppleX = 400
+local AppleY= 500
+local AppleSprite
 
 ----------------------------------------------------------
 -- OnStart
@@ -19,6 +26,9 @@ function ExampleScript:OnStart()
 
 Background = Entity.create_entity()
 local BgBackground = Entity.add_sprite_component(Background, assets.textures.Background,960 , 640, 0)
+
+   
+
    ------------------------------------------------------
     -- Create player1
     ------------------------------------------------------
@@ -26,7 +36,7 @@ local BgBackground = Entity.add_sprite_component(Background, assets.textures.Bac
 
     Entity.set_local_pos(player1, x, y)
 	
-    local playerSprite1 = Entity.add_sprite_component(player1, assets.textures.Debug, 32, 32, 10)
+    local playerSprite1 = Entity.add_sprite_component(player1, assets.textures.player1, 32, 32, 10)
     Sprite.set_columns(playerSprite1,1)
     -- PLAYER 1 PHYSICS
 
@@ -37,29 +47,89 @@ local BgBackground = Entity.add_sprite_component(Background, assets.textures.Bac
     Fysics.set_gravity_scale(player1, 0) 
 
       local tileSize = 64
-    local floorY = 600
+    local floorY = -67
+    local CeilingY = 650
+    local floorx = 300
+    
   
+    --Create Apple
+
+    Apple = Entity.create_entity()
+
+    Entity.set_local_pos(Apple, AppleX, AppleY)
+
+    local AppleSprite = Entity.add_sprite_component(Apple, assets.textures.Apple, 32, 32, 10)
+    Sprite.set_columns(AppleSprite,1)
+
+    
+    Entity.add_fysics_component(Apple, 2, true) -- dynamic body
+    --Fysics.add_sprite_collider(player1, false)
+    Fysics.add_sprite_collider(Apple,true,1)
+
+    Fysics.set_gravity_scale(Apple, 0) 
+
     -- pick texture for left / middle / right
 	------------------------------------------------------
 	local tex = "middle"
 
 	for i = 0, 30 do
-		local tile = Entity.create_entity()
+		local Top = Entity.create_entity()
+        local Bottom = Entity.create_entity()
+        local Left = Entity.create_entity()
+        local Right = Entity.create_entity()
 		local xPos = i * tileSize
+        local yPos = i * tileSize
 
+
+        --Bottom
 		------------------------------------------------------
 		-- place sprite
 		------------------------------------------------------
-		Entity.set_local_pos(tile, xPos, floorY)
-		local s = Entity.add_sprite_component(tile, assets.textures.Sand, tileSize, tileSize, 1)
+		Entity.set_local_pos(Bottom, xPos, floorY)
+	    local s = Entity.add_sprite_component(Bottom, assets.textures.sand, tileSize, tileSize, 1)
         Sprite.set_columns(s,1)
 		------------------------------------------------------
 		-- add physics body + collider
 		------------------------------------------------------
-		Entity.add_fysics_component(tile, 0, false)  -- static
-		Fysics.add_sprite_collider(tile, false,1)
+		Entity.add_fysics_component(Bottom, 0, false)  -- static
+		Fysics.add_sprite_collider(Bottom, false,1)
+
+        
+        --Top
+        
+        Entity.set_local_pos(Top, xPos, CeilingY)
+	    local s = Entity.add_sprite_component(Top, assets.textures.sand, tileSize, tileSize, 1)
+        Sprite.set_columns(s,1)
+		------------------------------------------------------
+		-- add physics body + collider
+		------------------------------------------------------
+		Entity.add_fysics_component(Top, 0, false)  -- static
+		Fysics.add_sprite_collider(Top, false,1)
+
+        --Left 
+        Entity.set_local_pos(Left, -67, yPos)
+	    local s = Entity.add_sprite_component(Left, assets.textures.sand, tileSize, tileSize, 1)
+        Sprite.set_columns(s,1)
+		------------------------------------------------------
+		-- add physics body + collider
+		------------------------------------------------------
+		Entity.add_fysics_component(Left, 0, false)  -- static
+		Fysics.add_sprite_collider(Left, false,1)
+
+
+        --Right
+        Entity.set_local_pos(Right, 967, yPos)
+	    local s = Entity.add_sprite_component(Right, assets.textures.sand, tileSize, tileSize, 1)
+        Sprite.set_columns(s,1)
+		------------------------------------------------------
+		-- add physics body + collider
+		------------------------------------------------------
+		Entity.add_fysics_component(Right, 0, false)  -- static
+		Fysics.add_sprite_collider(Right, false,1)
+
 
 end
+
 
 
 ----------------------------------------------------------
