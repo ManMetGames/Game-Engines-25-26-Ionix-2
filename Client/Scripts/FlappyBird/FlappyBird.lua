88920848ef17391904d34f5d2595413b9565944f
Lui <96396660+Taurinic@users.noreply.github.com>
2025-12-02 16:12:35 +0000

    ++

--- Client/Scripts/FlappyBird/FlappyBird.lua
+++ Client/Scripts/FlappyBird/FlappyBird.lua
@@ -15,7 +15,7 @@ local t = 10
 
 -- Scroll 
 local bgX = 0
-local bgScrollSpeed = -1  -- Scrolls to the left
+local bgScrollSpeed = -3  -- Scrolls to the left
 local bgWidth = 960       -- background width
 
 -- Pipe variables
@@ -48,21 +48,24 @@ function ExampleScript:OnStart()
     ------------------------------------------------------
     player1 = Entity.create_entity()
 
-    Entity.set_global_pos(player1, x, 300)
+    Entity.set_global_pos(player1, x, 600)
 	
     local playerSprite1 = Entity.add_sprite_component(player1, assets.textures.FlappyBird, 32, 32, 10)
     Sprite.set_columns(playerSprite1,1)
     -- PLAYER 1 PHYSICS
 
     Entity.add_fysics_component(player1, enums.bodytype.dynamicBody, true) -- dynamic body
+
     --Fysics.add_sprite_collider(player1, false)
     Fysics.add_sprite_collider(player1,false,1)
+
     -- Freeze bird
-    Fysics.set_gravity_scale(player1, 0)
+    Fysics.set_gravity_scale(player1, 1)
 
 
     local tileSize = 64
     local floorY = 600
+    
 	------------------------------------------------------
 	-- pick texture for left / middle / right
 	------------------------------------------------------
@@ -82,7 +85,7 @@ function ExampleScript:OnStart()
 		-- add physics body + collider
 		------------------------------------------------------
 		Entity.add_fysics_component(tile, enums.bodytype.staticBody, false)  -- static
-		Fysics.add_sprite_collider(tile, false,1)
+		Fysics.add_sprite_collider(tile, false, 1)
 	end
 
 	------------------------------------------------------
@@ -94,6 +97,7 @@ function ExampleScript:OnStart()
 
 	local pipeSprite = Entity.add_sprite_component(pipe, assets.textures.FlappyPipe, 80, 300, 0)
     Sprite.set_columns(pipeSprite,1)
+
 	-- Kinematic body so it moves but isn't affected by gravity
 	Entity.add_fysics_component(pipe, enums.bodytype.kinematicBody, false)
 	Fysics.add_sprite_collider(pipe, false,1)
@@ -103,13 +107,22 @@ function ExampleScript:OnStart()
     pipeT = Entity.create_entity()
 	Entity.set_global_pos(pipeT, 640, 0)
 
+    -- I need a way to rotate the pipe 
+   --Fysics.rotate_pos(pipeT, -180)
+
+ 
+   
+
 	local pipeSpriteT = Entity.add_sprite_component(pipeT,assets.textures.FlappyPipe , 80, 300, 0)
     Sprite.set_columns(pipeSpriteT,1)
+    
 	-- Kinematic body so it moves but isn't affected by gravity
 	Entity.add_fysics_component(pipeT, enums.bodytype.kinematicBody, false)
 	Fysics.add_sprite_collider(pipeT, false,1)
 
-    if Input.get_key_down(Keys.ionix_a) then
+    
+
+    if Input.get_key_held(Keys.ionix_a) then
         Entity.set_global_pos(pipe, xPos, floorY)
 	end
 end
@@ -119,6 +132,8 @@ end
 ----------------------------------------------------------
 function ExampleScript:OnUpdate()
 
+  -- BACKGROUND SCROLLING -- START
+  
   -- Scroll the background
     bgX = bgX + bgScrollSpeed
 
@@ -131,32 +146,39 @@ function ExampleScript:OnUpdate()
 
   -- Set background position
     Entity.set_global_pos(Background, bgX, 0)
+
+  -- BACKGROUND SCROLLING -- END
+
     -- get current velocity
     local vel1 = Fysics.get_linear_velocity(player1)
     local vy1 = Fysics.get_linear_velocity(pipe)
     local vy1 = Fysics.get_linear_velocity(pipeT)
+
     -- Constant rightward movement
     local vx = 0
     local vy1 = Mafs.get_vec_y(vel1)
 
-	if Input.get_key_down(Keys.ionix_space) then
+	if Input.get_key_held(Keys.ionix_space) then   
+
         -- Bird move if space is pressed (allow gravity)
         Fysics.set_gravity_scale(player1, 1)
         Fysics.set_linear_velocity(pipe, pipeSpeed, 0)
         Fysics.set_linear_velocity(pipeT, pipeSpeed, 0)
+
         -- Set velocity directly to cancel out falling momentum
-        vy1 = -5  -- Jump velocity for player1
+        vy1 = -2  -- Jump velocity for player1
 	end
 
     Fysics.set_linear_velocity(player1, vx, vy1)
-
-    -- Pipe movement
-    local pipePos = Fysics.get_pos(pipe)
-    local pipePos = Fysics.get_pos(pipeT)
-    if Mafs.get_vec_x(pipePos) < pipeOffScreenLeft then
-        Fysics.set_pos(pipe, pipeStartX, pipePos.y)
-        Fysics.set_pos(pipeT, pipeStartX, pipePos.y)
-     end
+     -- Pipe movement
+        local pipePos = Fysics.get_pos(pipe)
+        local pipePos = Fysics.get_pos(pipeT)
+        
+        if Mafs.get_vec_x(pipePos) < pipeOffScreenLeft then
+            Fysics.set_pos(pipe, pipeStartX, pipePos.y)
+            Fysics.set_pos(pipeT, pipeStartX, pipePos.y)
+        end
+   
      
 end
 
