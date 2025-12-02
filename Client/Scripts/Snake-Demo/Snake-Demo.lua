7eccc7cfc9de83d7fa043810e640224267bfc6ad
whirlstrike <c.c.j.w.cw@gmail.com>
2025-12-02 10:25:27 +0000

    diagonal movement fix
    ensures that the player cant move diagonally

--- Client/Scripts/Snake-Demo/Snake-Demo.lua
+++ Client/Scripts/Snake-Demo/Snake-Demo.lua
@@ -73,18 +73,22 @@ function ExampleScript:OnUpdate()
   -- basic movment using the WASD keys
     if Input.get_key_down(Keys.ionix_d) then
         vx = 5
+        vy = 0
          Fysics.set_linear_velocity(player1, vx, vy)
     end    
     if Input.get_key_down(Keys.ionix_a) then
         vx = -5
+        vy = 0
          Fysics.set_linear_velocity(player1, vx, vy)
     end
     if Input.get_key_down(Keys.ionix_w) then
         vy = -5
+        vx = 0
          Fysics.set_linear_velocity(player1, vx, vy)
     end    
     if Input.get_key_down(Keys.ionix_s) then
         vy = 5
+        vx = 0
          Fysics.set_linear_velocity(player1, vx, vy)
     end   
 
