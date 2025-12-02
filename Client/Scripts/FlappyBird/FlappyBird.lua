2b2389783bb1db3ed00f5a7d381d5d806136a5ac
Wyu03 <23686787@stu.mmu.ac.uk>
2025-12-02 00:00:49 +0000

    Fixed Pipe Sprite Not Moving
    Pipe sprite now moves left with colliders

--- Client/Scripts/FlappyBird/FlappyBird.lua
+++ Client/Scripts/FlappyBird/FlappyBird.lua
@@ -13,9 +13,9 @@ local t = 10
 -- Pipe variables
 local pipe
 local pipeT
-local pipeSpeed = -3
+local pipeSpeed = -180
 local pipeStartX = 900
-local pipeOffScreenLeft = -100
+local pipeOffScreenLeft = -1
 
 ----------------------------------------------------------
 -- OnStart
@@ -58,8 +58,7 @@ function ExampleScript:OnStart()
     -- PLAYER 1 PHYSICS
 
     Entity.add_fysics_component(player1, 2, false) -- dynamic body
-    --Fysics.add_sprite_collider(player1, false)
-    Fysics.add_sprite_collider(player1, false)
+    Fysics.add_sprite_collider(player1, true)
     -- Freeze bird
     Fysics.set_gravity_scale(player1, 0)
 
@@ -96,7 +95,7 @@ function ExampleScript:OnStart()
 	------------------------------------------------------
     --BOTTOM PIPE
 	pipe = Entity.create_entity()
-	Entity.set_entity_pos(pipe, 640, 400)
+	Entity.set_entity_pos(pipe, pipeStartX, 400)
 
 	local pipeSprite = Entity.add_sprite_component(pipe, "FlappyPipe", 80, 185, 0)
     Sprite.set_width(pipeSprite, 480)
@@ -107,10 +106,12 @@ function ExampleScript:OnStart()
 	Entity.add_fysics_component(pipe, 1, false)
 	Fysics.add_sprite_collider(pipe, false)
 
+    -- Position
+    Fysics.set_pos(pipe, pipeStartX / 100, 4)
 
     -- TOP PIPE
     pipeT = Entity.create_entity()
-	Entity.set_entity_pos(pipeT, 640, 0)
+	Entity.set_entity_pos(pipeT, pipeStartX, 0)
 
 	local pipeSpriteT = Entity.add_sprite_component(pipeT, "FlappyPipe2", 80, 185, 0)
     Sprite.set_width(pipeSpriteT, 480)
@@ -120,16 +121,21 @@ function ExampleScript:OnStart()
 	-- Kinematic body so it moves but isn't affected by gravity
 	Entity.add_fysics_component(pipeT, 1, false)
 	Fysics.add_sprite_collider(pipeT, false)
+
+    Fysics.set_pos(pipeT, pipeStartX / 100, 0)
 end
 
 ----------------------------------------------------------
 -- OnUpdate
 ----------------------------------------------------------
 function ExampleScript:OnUpdate()
-    -- get current velocity
+
+    ------------------------------------------------------
+	-- Player
+	------------------------------------------------------
+    -- Get current velocity
     local vel1 = Fysics.get_linear_velocity(player1)
-    local vy1 = Fysics.get_linear_velocity(pipe)
-    local vy1 = Fysics.get_linear_velocity(pipeT)
+
     -- Constant rightward movement
     local vx = 0
     local vy1 = vel1.y
@@ -137,30 +143,30 @@ function ExampleScript:OnUpdate()
 	if Input.get_key_down(Keys.ionix_space) then
         -- Bird move if space is pressed (allow gravity)
         Fysics.set_gravity_scale(player1, 1)
-        Fysics.set_linear_velocity(pipe, pipeSpeed, 0)
-        Fysics.set_linear_velocity(pipeT, pipeSpeed, 0)
+
         -- Set velocity directly to cancel out falling momentum
         vy1 = -5  -- Jump velocity for player1
 	end
 	
-	--if Input.get_key_down(Keys.ionix_a) then
-    --    Entity.set_entity_pos(pipe, xPos, floorY)
-	--end
-
     Fysics.set_linear_velocity(player1, vx, vy1)
+
+    ------------------------------------------------------
+	-- Pipes move left
+	------------------------------------------------------
+    local pipeS = pipeSpeed / 100
+    Fysics.set_linear_velocity(pipe, pipeS, 0)
+    Fysics.set_linear_velocity(pipeT, pipeS, 0)
 
     -- Pipe movement
     local pipePos = Fysics.get_pos(pipe)
     local pipePos = Fysics.get_pos(pipeT)
-    
-   -- if pipePos.x < pipeOffScreenLeft then
-       -- Fysics.set_pos(pipe, pipeStartX, pipePos.y)
-       -- Fysics.set_pos(pipeT, pipeStartX, pipePos.y)
-    --end
  	
-	if pipePos.x < 0 then
-        Entity.set_entity_pos(pipe, 640, 400)
-        Entity.set_entity_pos(pipeT, 640, 0)
+	if pipePos.x < pipeOffScreenLeft then
+        Fysics.set_pos(pipe, pipeStartX / 100, 4)
+        Fysics.set_pos(pipeT, pipeStartX / 100, 0)
+
+        Entity.set_entity_pos(pipe, pipeStartX, 400)
+        Entity.set_entity_pos(pipeT, pipeStartX, 0)
 	end
 end
 
