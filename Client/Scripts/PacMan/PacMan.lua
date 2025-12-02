a1b42284e1f438b7c5db35b72281b3c8b1200e1c
JoshuaJ123 <22512619@stu.mmu.ac.uk>
2025-12-02 15:43:11 +0000

    Initialize pacman into the game.

--- Client/Scripts/PacMan/PacMan.lua
+++ Client/Scripts/PacMan/PacMan.lua
@@ -3,21 +3,8 @@ local assets = require("Scripts.Assets")
 local enums = require("Scripts.Enums")
 local Background
 local player1
-local goal
-local playerSprite
-local goalSprite
-local x = 200
-local goalX = 500
-local goalY = 500
-local y = 300
-local t = 10
-
--- Pipe variables
-local pipe
-local pipeT
-local pipeSpeed = -3
-local pipeStartX = 900
-local pipeOffScreenLeft = -100
+local winWidth = Window.get_width()
+local winHeight = Window.get_height()
 
 ----------------------------------------------------------
 -- OnStart
@@ -30,17 +17,18 @@ function PacMan:OnStart()
     Background = Entity.create_entity()
     local BgBackground = Entity.add_sprite_component(Background, assets.textures.PacManBackground,960 , 640, 0)
     
-
     ------------------------------------------------------
     -- Create player1
     ------------------------------------------------------
     player1 = Entity.create_entity()
+    local pacman = Entity.add_sprite_component(player1, assets.textures.PacMan, 40, 40, 1)
 
-    Entity.set_global_pos(player1, x, 300)
+    Sprite.set_width(pacman, 840)
+    Sprite.set_height(pacman, 680)
+
+    Entity.set_global_pos(player1, winWidth/2-20,  winHeight/2+15)
 	
-
-    -- PLAYER 1 PHYSICS
-
+    -- PLAYER 1 PHYSICs
     Entity.add_fysics_component(player1, enums.bodytype.dynamicBody, true) -- dynamic body
     --Fysics.add_sprite_collider(player1, false)
     Fysics.add_sprite_collider(player1,false,1)
@@ -58,43 +46,19 @@ function PacMan:OnStart()
 	for i = 0, 30 do
 		local tile = Entity.create_entity()
 		local xPos = i * tileSize
-
-		------------------------------------------------------
-		-- place sprite
-		------------------------------------------------------
-		
 		------------------------------------------------------
 		-- add physics body + collider
 		------------------------------------------------------
 		Entity.add_fysics_component(tile, enums.bodytype.staticBody, false)  -- static
 		Fysics.add_sprite_collider(tile, false,1)
 	end
-
-	------------------------------------------------------
-	-- Create pipe obstacle
-	------------------------------------------------------
-  
 end
 
 ----------------------------------------------------------
 -- OnUpdate
 ----------------------------------------------------------
 function PacMan:OnUpdate()
-    -- get current velocity
-    local vel1 = Fysics.get_linear_velocity(player1)
-    
-    -- Constant rightward movement
-    local vx = 0
-    local vy1 = Mafs.get_vec_y(vel1)
 
-	
-
-    Fysics.set_linear_velocity(player1, vx, vy1)
-
-    -- Pipe movement
-   
-     
-     
 end
 
 return PacMan
