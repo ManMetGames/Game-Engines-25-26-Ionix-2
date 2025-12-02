4b37854beae731a58924d73d92f19223e5df982d
JoshCoulbeck <23656773@stu.mmu.ac.uk>
2025-12-02 10:49:06 +0000

    Added Snake sprite and new background to fit game

--- Client/Scripts/Snake-Demo/Snake-Demo.lua
+++ Client/Scripts/Snake-Demo/Snake-Demo.lua
@@ -10,7 +10,10 @@ local goalX = 500
 local goalY = 500
 local y = 300
 local t = 10
-
+local h = 500
+local w = 400
+local text = "Hello, World!"
+local font = assets.fonts.DefaultFont
 
 ----------------------------------------------------------
 -- OnStart
@@ -19,6 +22,9 @@ function ExampleScript:OnStart()
 
 Background = Entity.create_entity()
 local BgBackground = Entity.add_sprite_component(Background, assets.textures.Background,960 , 640, 0)
+
+   
+
    ------------------------------------------------------
     -- Create player1
     ------------------------------------------------------
@@ -26,7 +32,7 @@ local BgBackground = Entity.add_sprite_component(Background, assets.textures.Bac
 
     Entity.set_local_pos(player1, x, y)
 	
-    local playerSprite1 = Entity.add_sprite_component(player1, assets.textures.Debug, 32, 32, 10)
+    local playerSprite1 = Entity.add_sprite_component(player1, assets.textures.player1, 32, 32, 10)
     Sprite.set_columns(playerSprite1,1)
     -- PLAYER 1 PHYSICS
 
@@ -50,14 +56,14 @@ local BgBackground = Entity.add_sprite_component(Background, assets.textures.Bac
 		------------------------------------------------------
 		-- place sprite
 		------------------------------------------------------
-		Entity.set_local_pos(tile, xPos, floorY)
-		local s = Entity.add_sprite_component(tile, assets.textures.Sand, tileSize, tileSize, 1)
-        Sprite.set_columns(s,1)
+		--Entity.set_local_pos(tile, xPos, floorY)
+	--	local s = Entity.add_sprite_component(tile, assets.textures.Sand, tileSize, tileSize, 1)
+      --  Sprite.set_columns(s,1)
 		------------------------------------------------------
 		-- add physics body + collider
 		------------------------------------------------------
-		Entity.add_fysics_component(tile, 0, false)  -- static
-		Fysics.add_sprite_collider(tile, false,1)
+		--Entity.add_fysics_component(tile, 0, false)  -- static
+		--Fysics.add_sprite_collider(tile, false,1)
 
 end
 
@@ -69,6 +75,9 @@ function ExampleScript:OnUpdate()
 
     local vx = 0
     local vy = 0
+
+
+
 
   -- basic movment using the WASD keys
     if Input.get_key_held(Keys.ionix_d) then
