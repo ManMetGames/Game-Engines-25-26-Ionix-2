fcb0b414ffd1cbd139baed9a8e09ad20be8cea6c
whirlstrike <c.c.j.w.cw@gmail.com>
2025-12-02 09:52:23 +0000

    constant player movement
    the player will now be moving in a set direction once the key has been pressed once, at the moment it will only go left

--- Client/Scripts/Snake-Demo/Snake-Demo.lua
+++ Client/Scripts/Snake-Demo/Snake-Demo.lua
@@ -71,28 +71,11 @@ function ExampleScript:OnUpdate()
     local vy = 0
 
   -- basic movment using the WASD keys
-    if Input.get_key_held(Keys.ionix_d) then
-       
+    if Input.get_key_down(Keys.ionix_d) then
         vx = 5
-    end     
-
-        if Input.get_key_held(Keys.ionix_a) then
-       
-        vx = -5
-    end  
-
-
-        if Input.get_key_held(Keys.ionix_w) then
-       
-        vy = -5
-    end        
-
-        if Input.get_key_held(Keys.ionix_s) then
-       
-        vy = 5
-    end  
-
- Fysics.set_linear_velocity(player1, vx, vy)
+         Fysics.set_linear_velocity(player1, vx, vy)
+    end    
+   
      end
       
 end
