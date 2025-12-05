0f08fb69c51adb6aba756985110a16430812715d
Adam Faraj <76076640+AKorty7@users.noreply.github.com>
2025-12-05 00:45:54 +0000

    Fencing game

--- Client/Scripts/Settings.lua
+++ Client/Scripts/Settings.lua
@@ -15,8 +15,9 @@ LoadModule("Assets", "Scripts/Assets.lua")
 --LoadModule("Ball", "Scripts/Ball.lua")
 LoadModule("Assets", "Scripts/Assets.lua")
 LoadModule("Enums", "Scripts/Enums.lua")
-LoadModule("FlappyBird", "Scripts/FlappyBird/FlappyBird.lua")
+--LoadModule("FlappyBird", "Scripts/FlappyBird/FlappyBird.lua")
 --LoadModule("ExampleScript", "Scripts/ExampleScript.lua")
+LoadModule("FencingGame", "Scripts/Fencing game/FencingGame.Lua")
 
 -- Lifecycle hooks
 function OnStart()
