78ab552d25b65c1620c0a9407c075951e0240317
MMU-Jimmy <156657077+MMU-Jimmy@users.noreply.github.com>
2025-12-06 08:31:24 +0000

    Map files

--- Client/Ionix 2 - Editor/Assets/Jimmy Demo/Background.tsx
+++ Client/Ionix 2 - Editor/Assets/Jimmy Demo/Background.tsx
@@ -1,2 +1,25 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Background" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset version="1.10" tiledversion="1.11.2" name="Background" tilewidth="64" tileheight="64" tilecount="7" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="Background/Blue.png" width="64" height="64"/>
+ </tile>
+ <tile id="1">
+  <image source="Background/Brown.png" width="64" height="64"/>
+ </tile>
+ <tile id="2">
+  <image source="Background/Gray.png" width="64" height="64"/>
+ </tile>
+ <tile id="3">
+  <image source="Background/Green.png" width="64" height="64"/>
+ </tile>
+ <tile id="4">
+  <image source="Background/Pink.png" width="64" height="64"/>
+ </tile>
+ <tile id="5">
+  <image source="Background/Purple.png" width="64" height="64"/>
+ </tile>
+ <tile id="6">
+  <image source="Background/Yellow.png" width="64" height="64"/>
+ </tile>
+</tileset>
