f534aaf492782407bdd9c6ed652d1210e6099e2d
Zul <146729926+Zulfaqaar@users.noreply.github.com>
2025-12-09 11:07:03 +0000

    Highscore json file functions
    Co-Authored-By: VSky26 <183742499+VSky26@users.noreply.github.com>
    Co-Authored-By: Wendy Yu <146729946+Wyu03@users.noreply.github.com>

---
+++ Engine/src/json.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "json.hpp"
+#include <fstream>
+
+namespace IonixEngine
+{
+    using json = nlohmann::json;
+
+    void SaveHighScore(int score) {
+        json data;
+        data["highscore"] = score;
+
+        std::ofstream file("highscore.json");
+        file << data.dump(4); // pretty print with indent 4
+    }
+
+    int LoadHighScore() {
+        std::ifstream file("highscore.json");
+        if (!file.is_open())
+            return 0; // default if no file exists
+
+        json data;
+        file >> data;
+
+        return data.value("highscore", 0);
+    }
+}
