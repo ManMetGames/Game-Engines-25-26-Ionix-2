91a38e8d75e974ad2a596120dfc649e688d45e5a
Zoe <zoekive7@gmail.com>
2025-12-09 16:15:18 +0000

    code works

--- Client/Scripts/Peggle/peggle.lua
+++ Client/Scripts/Peggle/peggle.lua
@@ -1,7 +1,13 @@
 local Peggle = {}
 
+local assets = require("Scripts.Assets")
+local enums = require("Scripts.Enums")
+
+local ball
+
 function Peggle:OnStart()
-    print("Peggle is GO!")
+    ball = Entity.create_entity()
+    Entity.add_sprite_component(ball, assets.textures.PimBall, 50, 50, 0)
 end
 
 return Peggle
