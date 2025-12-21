e166cdf3ea54168783d369f4b9bb2bbd41960789
Zul <22528471@stu.mmu.ac.uk>
2025-12-21 05:25:49 +0000

    Set game to System Shooter

--- Client/Scripts/Settings.lua
+++ Client/Scripts/Settings.lua
@@ -22,14 +22,14 @@ end
 --LoadModule("EntityPlayer", "Scripts/EntityPlayer.lua")
 --LoadModule("Assets", "Scripts/Assets.lua")
 --LoadModule("Enums", "Scripts/Enums.lua")
-LoadModule("FlappyBird", "Scripts/FlappyBird/FlappyBird.lua")
+--LoadModule("FlappyBird", "Scripts/FlappyBird/FlappyBird.lua")
 --LoadModule("CorutineTest", "Scripts/TestingCoroutine/CoroutineTest.lua")
 --LoadModule("ExampleScript", "Scripts/ExampleScript.lua")
 --LoadModule("Ball", "Scripts/Ball.lua")
 --LoadModule("FlappyBird", "Scripts/FlappyBird/FlappyBird.lua")
 --LoadModule("ExampleScript", "Scripts/ExampleScript.lua")
 --LoadModule("Ball","Scripts/Ball.lua")
---LoadModule("SystemShooter", "Scripts/SystemShooter/SystemShooter.lua")
+LoadModule("SystemShooter", "Scripts/SystemShooter/SystemShooter.lua")
 
 -- Lifecycle hooks
 function OnStart()
