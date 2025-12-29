8e421e9f27a43074816849de0903278dc306c665
Michalowsk1 <146729959+Michalowsk1@users.noreply.github.com>
2025-12-29 13:54:23 +0000

    PacMan Map Generator
    -Got rid of/adjusted flappyBird code to support my PacMan game
    -Added a function which makes a map that can be easily adjustable
    
    Co-Authored-By: Tia <146729907+Glowflye@users.noreply.github.com>

--- Client/Scripts/PacMan/PacMan.lua
+++ Client/Scripts/PacMan/PacMan.lua
@@ -1,124 +1,57 @@
 local ExampleScript = {}
 local assets = require("Scripts.Assets")
 local enums = require("Scripts.Enums")
-local Background
 local player1
-local goal
-local playerSprite
-local goalSprite
 local x = 200
-local goalX = 500
-local goalY = 500
 local y = 300
-local t = 10
-
--- Pipe variables
-local pipe
-local pipeT
-local pipeSpeed = -3
-local pipeStartX = 900
-local pipeOffScreenLeft = -100
-
---PacMan
-local xDir -- -1 goes left, 1 goes right
-local yDir -- -1 goes up, 1 goes down
-
-local windowSize
 
 ----------------------------------------------------------
 -- OnStart
 ----------------------------------------------------------
 function ExampleScript:OnStart()
+    local tileSize = 128
+    local YPos = Window.get_height()/6
 
-    local pie = Mafs.pi()
-    windowSize = Window.get_height()
-    print(windowSize)    
-    ------------------------------------------------------
-	-- Background Texture
-	------------------------------------------------------
-    Background = Entity.create_entity()
-    local BgBackground = Entity.add_sprite_component(Background, assets.textures.Background,960 , 640, 0)
-    
-
-    ------------------------------------------------------
+    	------------------------------------------------------
+		-- makes map by looping through and making a 3x3 map grid, can be easily adjustable
+		------------------------------------------------------
+	for i = 1, 3 do
+        for j = 1, 3 do
+		local tile = Entity.create_entity()
+        local xPos = (j * tileSize) + 128
+		------------------------------------------------------
+		-- place sprite
+		------------------------------------------------------
+		Entity.set_global_pos(tile, xPos, YPos)
+		local s = Entity.add_sprite_component(tile, assets.textures.MapBox, tileSize, tileSize, 1)
+        Sprite.set_columns(s,1)
+		------------------------------------------------------
+		-- add physics body + collider
+		------------------------------------------------------
+		Entity.add_fysics_component(tile, enums.bodytype.staticBody, false)  -- static
+		Fysics.add_sprite_collider(tile, true, 1)
+        end
+        YPos = YPos + tileSize
+	end
+        ------------------------------------------------------
     -- Create player1
     ------------------------------------------------------
     player1 = Entity.create_entity()
-    collider = Entity.create_entity()
 
     Entity.set_global_pos(player1, x, y)
-    Entity.set_global_pos(collider, x, 300)
-
-
 	
     local playerSprite1 = Entity.add_sprite_component(player1, assets.textures.PacMan, 32, 32, 10) --Pac man sprite render + animation
     Sprite.set_columns(playerSprite1, 2)
     Sprite.rows(playerSprite1, 2)
     Sprite.set_height(playerSprite1, 32)
     Sprite.set_width(playerSprite1, 32)
-    Sprite.set_playback_mode(playerSprite1, 4)
+    Sprite.set_playback_mode(playerSprite1, 3)
+
     -- PLAYER 1 PHYSICS
 
     Entity.add_fysics_component(player1, enums.bodytype.dynamicBody, true) -- dynamic body, last value is rotation lock
-    --Entity.add_fysics_component(collider, enums.bodytype.dynamicBody, true) -- dynamic body, last value is rotation lock
-
-    --Fysics.add_sprite_collider(player1, false)
     Fysics.add_sprite_collider(player1,false,1)
-    -- Freeze bird
     Fysics.set_gravity_scale(player1, 0)
-
-
-    local tileSize = 64
-    local floorY = 600
-	------------------------------------------------------
-	-- pick texture for left / middle / right
-	------------------------------------------------------
-	local tex = "middle"
-
-	for i = 0, 30 do
-		local tile = Entity.create_entity()
-		local xPos = i * tileSize
-
-		------------------------------------------------------
-		-- place sprite
-		------------------------------------------------------
-		Entity.set_global_pos(tile, xPos, floorY)
-		local s = Entity.add_sprite_component(tile, assets.textures.Sand, tileSize, tileSize, 1)
-        Sprite.set_columns(s,1)
-		------------------------------------------------------
-		-- add physics body + collider
-		------------------------------------------------------
-		Entity.add_fysics_component(tile, enums.bodytype.staticBody, false)  -- static
-		Fysics.add_sprite_collider(tile, false,1)
-	end
-
-	------------------------------------------------------
-	-- Create pipe obstacle
-	------------------------------------------------------
-    --BOTTOM PIPE
-	pipe = Entity.create_entity()
-	Entity.set_global_pos(pipe, 640, 400)
-
-	local pipeSprite = Entity.add_sprite_component(pipe, assets.textures.FlappyPipe, 80, 300, 0)
-    Sprite.set_columns(pipeSprite,1)
-	-- Kinematic body so it moves but isn't affected by gravity
-	Entity.add_fysics_component(pipe, enums.bodytype.kinematicBody, false)
-	Fysics.add_sprite_collider(pipe, false,1)
-
-
-    -- TOP PIPE
-    pipeT = Entity.create_entity()
-	Entity.set_global_pos(pipeT, 640, 0)
-
-	local pipeSpriteT = Entity.add_sprite_component(pipeT,assets.textures.FlappyPipe , 80, 300, 0)
-    Sprite.set_columns(pipeSpriteT,1)
-	-- Kinematic body so it moves but isn't affected by gravity
-	Entity.add_fysics_component(pipeT, enums.bodytype.kinematicBody, false)
-	Fysics.add_sprite_collider(pipeT, false,1)
-
-    if Input.get_key_down(Keys.ionix_a) then
-        Entity.set_global_pos(pipe, xPos, floorY)
-	end
 end
 
 ----------------------------------------------------------
@@ -127,8 +60,6 @@ end
 function ExampleScript:OnUpdate()
     -- get current velocity
     local vel1 = Fysics.get_linear_velocity(player1)
-    local vy1 = Fysics.get_linear_velocity(pipe)
-    local vy1 = Fysics.get_linear_velocity(pipeT)
     -- Constant rightward movement
     local vx = Mafs.get_vec_x(vel1);
     local vy1 = Mafs.get_vec_y(vel1)
@@ -136,41 +67,22 @@ function ExampleScript:OnUpdate()
     --if Input.get_key_down(Kyes.ionix_d) then Fysics.set_linear_velocity(player1, 20, 0) end
 
 	if Input.get_key_down(Keys.ionix_w) then -- move up
-        vy1 = -1  
+        vy1 = -1
         vx = 0
-        --Fysics.set_angle(player1, 270 * (Mafs.pi() / 180))
-        Entity.set_global_rot(player1, 270)
-        Entity.set_global_pos(player1, 500, 200)
 	end
     if Input.get_key_down(Keys.ionix_s) then -- move down
          vy1 = 1
           vx = 0
-        --Fysics.set_angle(player1, 90 * (Mafs.pi() / 180))
-        Entity.set_global_rot(player1, 180)
     end
     if Input.get_key_down(Keys.ionix_a) then -- move left
         vx = -1
         vy1 = 0
-        --Fysics.set_angle(player1, Mafs.pi())
-        Entity.set_global_rot(player1, 90)
     end
     if Input.get_key_down(Keys.ionix_d) then -- move right
         vx = 1
         vy1 = 0
-        Entity.set_global_rot(player1, 0)
     end
-
-
     Fysics.set_linear_velocity(player1, vx, vy1)
-
-    -- Pipe movement
-    local pipePos = Fysics.get_pos(pipe)
-    local pipePos = Fysics.get_pos(pipeT)
-    if Mafs.get_vec_x(pipePos) < pipeOffScreenLeft then
-        Fysics.set_pos(pipe, pipeStartX, pipePos.y)
-        Fysics.set_pos(pipeT, pipeStartX, pipePos.y)
-     end
-     
 end
 
 return ExampleScript
