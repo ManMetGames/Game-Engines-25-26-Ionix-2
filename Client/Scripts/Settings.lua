7a075cd78750342a595405fae7ca6acb8d19f9f8
Adam Faraj <76076640+AKorty7@users.noreply.github.com>
2025-12-30 04:49:28 +0000

    Made so the Spores Spread at 10fps and also have colour (ONE SPORE COLONY)

--- Client/Scripts/Settings.lua
+++ Client/Scripts/Settings.lua
@@ -29,7 +29,8 @@ end
 --LoadModule("FlappyBird", "Scripts/FlappyBird/FlappyBird.lua")
 --LoadModule("ExampleScript", "Scripts/ExampleScript.lua")
 --LoadModule("Ball","Scripts/Ball.lua")
-LoadModule("SystemShooter", "Scripts/SystemShooter/SystemShooter.lua")
+--LoadModule("SystemShooter", "Scripts/SystemShooter/SystemShooter.lua")
+LoadModule("SporeAutomata", "Scripts/SporeAutomata/SporeAutomata.lua")
 
 -- Lifecycle hooks
 function OnStart()
--- Client/Scripts/SporeAutomata/SporeAutomata.lua
+++ Client/Scripts/SporeAutomata/SporeAutomata.lua
@@ -1,10 +1,17 @@
 local spore = {}
 
-function spore:OnStart()
-    print("[Spore] Spore automata initialized.")
-    
+function spore:OnStart()    
     -- Initialize grid (example: 64x64)
-    self.gridSize = 64
+    
+    self.gridSize = 16
+    self.EMPTY  = 0
+    self.RED    = 1
+    self.BLUE   = 2
+    self.GREEN  = 3
+    self.PURPLE = 4
+    self.YELLOW = 5
+
+    
     self.grid = {}
     for y = 1, self.gridSize do
         self.grid[y] = {}
@@ -20,9 +27,59 @@ function spore:OnStart()
         local y = math.random(1, self.gridSize)
         self.grid[y][x] = 1  -- Type A spore
     end
+
+    self.tileSize = 32
+    self.tiles = {}
+    self.tileSprites = {}
+
+    print("[Spore] Spore automata initialized.")
+    local assets = require("Scripts.Assets")
+
+    for y = 1, self.gridSize do
+        self.tiles[y] = {}
+        self.tileSprites[y] = {}
+        for x = 1, self.gridSize do
+            local e = Entity.create_entity()
+            Entity.set_global_pos(e,(x-1) * self.tileSize,(y-1) * self.tileSize)
+            local s = Entity.add_sprite_component(e,assets.textures.Debug, self.tileSize, self.tileSize, 10)
+            Sprite.set_playback_mode(s, 4)
+            self.tiles[y][x] = e
+            self.tileSprites[y][x] = s
+            
+        end
+    
+    end
+
+    self.accum = 0
+    self.step = 0.1
+ 
+    self:RenderGrid()
+end 
+
+
+function spore:RenderGrid()
+    for y = 1, self.gridSize do 
+        for x = 1, self.gridSize do
+            if self.grid[y][x] == 1 then
+                Sprite.set_color(self.tileSprites[y][x], 255, 0, 0) -- red spore
+            else
+                Sprite.set_color(self.tileSprites[y][x], 25, 0, 0) -- Black tile
+            end
+            
+        end
+    end 
 end
 
+
+
 function spore:OnUpdate()
+
+    self.accum = self.accum + Mafs.delta_time()
+    if self.accum < self.step then
+        return
+    end
+    self.accum = self.accum - self.step
+
     -- Simple cellular automata update
     local newGrid = {}
     
@@ -61,6 +118,7 @@ function spore:OnUpdate()
     end
     
     self.grid = newGrid
+    self:RenderGrid()
 end
 
 return spore
