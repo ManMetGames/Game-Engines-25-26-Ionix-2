e9d0245b37d4b5c092ae7fbed6861c05edfd7063
Muhammad-Butt-Games <21391319@stu.mmu.ac.uk>
2026-01-08 15:00:37 +0000

    Added Floor
    Added floor to game

--- Client/Scripts/Game/Game.lua
+++ Client/Scripts/Game/Game.lua
@@ -3,6 +3,10 @@ local assets = require("Scripts.Assets")
 local enums = require("Scripts.Enums")
 --Game Objects
 local Background
+local Floor
+--Floor Settings
+local FloorY = 500
+local FloorHeight = 20
 local x = 200
 local y = 300
 local t = 10
@@ -17,6 +21,12 @@ function ExampleScript:OnStart()
     Background = Entity.create_entity()
     local BgBackground = Entity.add_sprite_component(Background, assets.textures.Background,960 , 640, 0)
     Entity.set_global_pos(Background, 480, 320)
+
+    -- Create Floor
+    Floor = Entity.create_entity()
+    Entity.add_sprite_component(Floor, assets.textures.Background, 960, floorHeight, 1)
+    Entity.set_global_pos(Floor, 480, floorY)
+
 
     ------------------------------------------------------
     -- Create player1
