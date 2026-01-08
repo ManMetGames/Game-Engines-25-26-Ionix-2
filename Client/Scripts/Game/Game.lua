2671eb1afe431ccbe47c0decfb5302247c5bbab9
Muhammad-Butt-Games <21391319@stu.mmu.ac.uk>
2026-01-08 14:56:29 +0000

    Set background

--- Client/Scripts/Game/Game.lua
+++ Client/Scripts/Game/Game.lua
@@ -1,6 +1,7 @@
 local ExampleScript = {}
 local assets = require("Scripts.Assets")
 local enums = require("Scripts.Enums")
+--Game Objects
 local Background
 local x = 200
 local y = 300
@@ -12,12 +13,10 @@ local coinCount = 0
 ----------------------------------------------------------
 function ExampleScript:OnStart()
 
-    ------------------------------------------------------
-	-- Background Texture
-	------------------------------------------------------
+    --Create Background
     Background = Entity.create_entity()
     local BgBackground = Entity.add_sprite_component(Background, assets.textures.Background,960 , 640, 0)
-    
+    Entity.set_global_pos(Background, 480, 320)
 
     ------------------------------------------------------
     -- Create player1
