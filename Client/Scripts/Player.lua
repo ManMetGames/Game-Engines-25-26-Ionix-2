77a45721dbc0d2b50a5635202e566b1235b14cb5
ScttRlph <23689704@stu.mmu.ac.uk>
2026-01-08 17:39:49 +0000

    Added Comments

--- Client/Scripts/Player.lua
+++ Client/Scripts/Player.lua
@@ -9,20 +9,23 @@ local y =  200
 local attackDamage = 10
 
 function player:OnStart()
-health = maxHealth
+health = maxHealth --Sets max health of the player on launch 
 
 Texture.add_texture("./Assets/"playerBlue_walk1", "player")
 
-player = Entity.create_entity()
+--creates player entity
+player = Entity.create_entity() 
 Entity.set_entity_pos(player, x , y) 
 playerSprite = Entity.add_sprite_component(player, "walk" , 75, 75 ,0)
 Sprite.set_playback_mode(playerSprite, 4) 
 
+--adds collision to player
 Entity.add_fysics_component(player, 2 , true) 
 Fysics.add_sprite_collider(player, true)
 
 end
 
+--allows player to attack the enemy
 function player:attack()
  if Input.get_mouse_button_down() then
 	enemy.health - attackDamage = enemy.dmgHealth
