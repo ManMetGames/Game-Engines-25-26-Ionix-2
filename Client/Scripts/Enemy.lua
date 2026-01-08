16ad5ad951f1980ae9bfc65b09c761ff5b20dcd0
ScttRlph <23689704@stu.mmu.ac.uk>
2026-01-08 13:44:48 +0000

    Added Basic Health to Enemy class

--- Client/Scripts/Enemy.lua
+++ Client/Scripts/Enemy.lua
@@ -1,6 +1,18 @@
 local enemy = {health = 100}
 	
+local enemy
+local maxHealth = 100 
+local x =500
+local y = 600
+	
+	
 function enemy:OnStart()
+	
+	Texture.add_texture("./Assets/ball.png", "enemy")
+	
+	enemy = Entity.create_entity()
+	Entity.set_entity_pos(enemy, x, y) 
+	enemySprite = Entity.add_sprite_component(enemy, "ball", 75, 75, 0) 
 	
 end
 
